7ec5a4135d7045f1207cc7c04331e74b050a34f9
Nikan <nikan.doosti@outlook.com>
2022-04-27 16:45:50 +0430

    clean code to use modular/functional codes

--- temp.py
+++ temp.py
@@ -1,34 +1,26 @@
-
-from utils import XFAPDF
-
-xml_file_path = 'sample/xml/5645e-17.xml'
+from utils.PDFIO import CanadaXFA
+from utils.constant import DOC_TYPES
+from utils import functional
+from utils import CANADA_5257E_KEY_ABBREVIATION
 
 src_path = 'sample/enc_sample/'  # path to source encrypted pdf
 des_path = 'sample/dec_sample/'  # path to decrypted pdf
+SAMPLE = 'sample/dec_sample/imm5645e.pdf'
 
-SAMPLE = 'sample/dec_sample/imm5257e.pdf'
+# Canada PDF to XML
+canada_xfa = CanadaXFA()
+canada_xfa.make_machine_readable(
+    file_name=None, src_path=src_path, des_path=des_path)
+xml = canada_xfa.extract_raw_content(SAMPLE)
 
-xfa_pdf = XFAPDF()
+# XFA to XML
+xml = canada_xfa.clean_xml_for_csv(xml=xml, type=DOC_TYPES.canada_5645e)
+# XML to flattened dict
+data_dict = canada_xfa.xml_to_flattened_dict(xml=xml)
+# clean flattened dict
+data_dict = functional.dict_summarizer(data_dict, cutoff_term='form1',
+                                       KEY_ABBREVIATION_DICT=CANADA_5257E_KEY_ABBREVIATION,
+                                       VALUE_ABBREVIATION_DICT=None)
+# write to csv
+functional.dict_to_csv(d=data_dict, path='sample/csv/5645e.csv')
 
-xfa_pdf.make_machine_readable(file_name=None, src_path=src_path, des_path=des_path)
-xml = xfa_pdf.extract_raw_content(SAMPLE)
-
-# remove bad characters
-import re
-xml_cleaned = re.sub(r"b'\\n", '', xml)
-xml_cleaned = re.sub(r"'", '', xml_cleaned)
-xml_cleaned = re.sub(r"\\n", '', xml_cleaned)
-
-# remove 9000 lines of redundant info for '5257e' doc
-import xml.etree.ElementTree as et
-tree = et.ElementTree()
-tree.parse('sample/xml/5257e-9-cleaned.xml')
-root = tree.getroot()
-junk = tree.findall('LOVFile')  
-root.remove(junk[0])
-xml_cleaned2 = str(et.tostring(root, encoding='utf8', method='xml'))
-xml_cleaned2 = re.sub(r"b'<\?xml version=\\'1.0\\' encoding=\\'utf8\\'\?>", '', xml_cleaned2)
-xml_cleaned2 = re.sub(r"'", '', xml_cleaned2)
-xml_cleaned2 = re.sub(r"\\n[ ]*", '', xml_cleaned2)
-
-print
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,7 @@
 from .PDFIO import PDFIO
 from .PDFIO import XFAPDF
+from .PDFIO import CanadaXFA
+from .PDFIO import DOC_TYPES
+
+from .constant import CANADA_5257E_KEY_ABBREVIATION
+from .constant import DOC_TYPES
