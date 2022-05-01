454fc4cde8dcf2da9fa8ea3b0d80339cf8c5bbb7
Nikronic <nikan.doosti@outlook.com>
2022-05-01 11:39:25 +0430

    finalize basic preprocessing of 5257e doc [#1, #2 needs to be implemented]

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ from utils.PDFIO import CanadaXFA
 from utils.constant import DOC_TYPES
 from utils import functional
 from utils.preprocessor import *
-from utils import CANADA_5257E_KEY_ABBREVIATION, CANADA_5257E_VALUE_ABBREVIATION
-from utils import CANADA_5257E_DROP_COLUMNS
-from utils import CANADA_CUTOFF_TERMS
+from utils import *
 
 import pandas as pd
 
@@ -46,7 +44,6 @@ dataframe.drop(CANADA_5257E_DROP_COLUMNS, axis=1, inplace=True)
 
 
 # column dtypes
-print
 # age to integer
 dataframe['P1.Age'] = dataframe['P1.Age'].astype('int16')
 # Adult binary state: adult=True or child=False
@@ -100,7 +97,8 @@ dataframe['P1.PD.PrevCOR.Row2.Country'] = dataframe['P1.PD.PrevCOR.Row2.Country'
 dataframe['P1.PD.PrevCOR.Row2.Status'] = dataframe['P1.PD.PrevCOR.Row2.Status'].astype(
     'string').fillna('0')
 # previous country of residency 02 period (P1.PD.PrevCOR.Row2): none -> random date -> int days
-fillna_datetime(dataframe=dataframe, col_base_name='P1.PD.PrevCOR.Row2')
+dataframe = fillna_datetime(
+    dataframe=dataframe, col_base_name='P1.PD.PrevCOR.Row2')
 dataframe = aggregate_datetime(dataframe=dataframe, col_base_name='P1.PD.PrevCOR.Row2',
                                new_col_name='Period', reference_date=None,
                                current_date=None)
@@ -111,7 +109,8 @@ dataframe['P1.PD.PrevCOR.Row3.Country'] = dataframe['P1.PD.PrevCOR.Row3.Country'
 dataframe['P1.PD.PrevCOR.Row3.Status'] = dataframe['P1.PD.PrevCOR.Row3.Status'].astype(
     'string').fillna('0')
 # previous country of residency 03 period (P1.PD.PrevCOR.Row3): none -> random date -> int days
-fillna_datetime(dataframe=dataframe, col_base_name='P1.PD.PrevCOR.Row3')
+dataframe = fillna_datetime(
+    dataframe=dataframe, col_base_name='P1.PD.PrevCOR.Row3')
 dataframe = aggregate_datetime(dataframe=dataframe, col_base_name='P1.PD.PrevCOR.Row3',
                                new_col_name='Period', reference_date=None,
                                current_date=None)
@@ -126,7 +125,7 @@ dataframe['P1.PD.CWA.Row2.Country'] = dataframe['P1.PD.CWA.Row2.Country'].astype
 # country where applying status: string -> categorical
 dataframe['P1.PD.CWA.Row2.Status'] = dataframe['P1.PD.CWA.Row2.Status'].astype(
     'string')
-# country where applying other: string -> categorical, maybe delete entirely
+# country where applying other: string -> categorical, maybe delete enitrely
 dataframe['P1.PD.CWA.Row2.Other'] = dataframe['P1.PD.CWA.Row2.Other'].astype(
     'string')
 # country where applying period: datetime -> int days
@@ -144,18 +143,160 @@ column_dropper(dataframe=dataframe, string='P1.MS.SecA.MarrDate.From')
 dataframe['P2.MS.SecA.PrevMarrIndicator'] = dataframe['P2.MS.SecA.PrevMarrIndicator'].apply(
     lambda x: True if x == 'Y' else False)
 
-# FIXME: doesn't fill value at all and when it does, it is NaT rather than correct `date``
 # previous spouse age: none -> random date -> int days
-# fillna_datetime(dataframe=dataframe, date=dataframe['P3.Sign.C1CertificateIssueDate'],
-#                 col_base_name='P2.MS.SecA.PrevSpouseDOB.DOBYear', one_sided=True)
-# dataframe = aggregate_datetime(dataframe=dataframe, col_base_name='P2.MS.SecA.PrevSpouseDOB.DOBYear',
-#                                new_col_name='Period', reference_date=None, one_sided='right',
-#                                current_date=dataframe['P3.Sign.C1CertificateIssueDate'], )
+dataframe = fillna_datetime(dataframe=dataframe, date=dataframe['P3.Sign.C1CertificateIssueDate'],
+                            col_base_name='P2.MS.SecA.PrevSpouseDOB.DOBYear', one_sided=False)
+dataframe = aggregate_datetime(dataframe=dataframe, col_base_name='P2.MS.SecA.PrevSpouseDOB.DOBYear',
+                               new_col_name='Period', reference_date=None, one_sided='right',
+                               current_date=dataframe['P3.Sign.C1CertificateIssueDate'], )
 
 # previous marriage period: none -> random date -> int days
-fillna_datetime(dataframe=dataframe, col_base_name='P2.MS.SecA')
+dataframe = fillna_datetime(dataframe=dataframe, col_base_name='P2.MS.SecA')
 dataframe = aggregate_datetime(dataframe=dataframe, col_base_name='P2.MS.SecA',
                                new_col_name='Period', reference_date=None,
                                current_date=None)
 # delete tnx to P2.MS.SecA.FromDate and P2.MS.SecA.ToDate.ToDate
 column_dropper(dataframe=dataframe, string='P2.MS.SecA.Prevly')
+# passport country of issue: string -> categorical
+dataframe['P2.MS.SecA.Psprt.CountryofIssue.CountryofIssue'] = dataframe['P2.MS.SecA.Psprt.CountryofIssue.CountryofIssue'].astype(
+    'string')
+# expiray remaining period: datetime -> int days
+dataframe = aggregate_datetime(dataframe=dataframe, col_base_name='P2.MS.SecA.Psprt.ExpiryDate',
+                               one_sided='left', new_col_name='Remaining',
+                               reference_date=dataframe['P3.Sign.C1CertificateIssueDate'])
+# Taiwan doc: bool -> binary
+dataframe['P2.MS.SecA.Psprt.TaiwanPIN'] = dataframe['P2.MS.SecA.Psprt.TaiwanPIN'].apply(
+    lambda x: True if (x is not None) and (x == 'Y') else False)
+# Isreal doc: bool -> binary
+dataframe['P2.MS.SecA.Psprt.IsraelPsprtIndicator'] = dataframe['P2.MS.SecA.Psprt.IsraelPsprtIndicator'].apply(
+    lambda x: True if (x is not None) and (x == 'Y') else False)
+# native lang: string -> categorical
+dataframe['P2.MS.SecA.Langs.languages.nativeLang.nativeLang'] = dataframe['P2.MS.SecA.Langs.languages.nativeLang.nativeLang'].astype(
+    'string')
+# communication lang: Eng, Fren, both, none -> categorical
+dataframe['P2.MS.SecA.Langs.languages.ableToCommunicate.ableToCommunicate'] = dataframe['P2.MS.SecA.Langs.languages.ableToCommunicate.ableToCommunicate'].astype(
+    'string')
+# language official test: bool -> binary
+dataframe['P2.MS.SecA.Langs.LangTest'] = dataframe['P2.MS.SecA.Langs.LangTest'].apply(
+    lambda x: True if x == 'Y' else False)
+# have national ID: bool -> binary
+dataframe['P2.natID.q1.natIDIndicator'] = dataframe['P2.natID.q1.natIDIndicator'].apply(
+    lambda x: True if x == 'Y' else False)
+# national ID country of issue: string -> categorical
+dataframe['P2.natID.natIDdocs.CountryofIssue.CountryofIssue'] = dataframe['P2.natID.natIDdocs.CountryofIssue.CountryofIssue'].astype(
+    'string')
+# United States doc: bool -> binary
+dataframe['P2.USCard.q1.usCardIndicator'] = dataframe['P2.USCard.q1.usCardIndicator'].apply(
+    lambda x: True if x == 'Y' else False)
+# drop contact information except having US Canada phone number
+column_dropper(dataframe=dataframe, string='P2.CI.cntct', exclude='CanadaUS')
+# US Canada phone number: bool -> binary
+dataframe['P2.CI.cntct.PhnNums.Phn.CanadaUS'] = dataframe['P2.CI.cntct.PhnNums.Phn.CanadaUS'].apply(
+    lambda x: True if x == '1' else False)
+# US Canada alt phone number: bool -> binary
+dataframe['P2.CI.cntct.PhnNums.AltPhn.CanadaUS'] = dataframe['P2.CI.cntct.PhnNums.AltPhn.CanadaUS'].apply(
+    lambda x: True if x == '1' else False)
+# purpose of visit: string, 8 states -> categorical
+dataframe['P3.DOV.PrpsRow1.PrpsOfVisit.PrpsOfVisit'] = dataframe['P3.DOV.PrpsRow1.PrpsOfVisit.PrpsOfVisit'].astype(
+    'string')
+# purpose of visit description: string -> binary
+dataframe['P3.DOV.PrpsRow1.Other.Other'] = dataframe['P3.DOV.PrpsRow1.Other.Other'].apply(
+    lambda x: True if x is not None else False)
+# how long going to stay: datetime -> int days
+dataframe = aggregate_datetime(dataframe=dataframe, col_base_name='P3.DOV.PrpsRow1.HLS',
+                               new_col_name='Period')
+# delete tnx to P3.DOV.PrpsRow1.HLS
+column_dropper(dataframe=dataframe, string='P3.DOV.PrpsRow1.HLS')
+# fund to integer
+dataframe['P3.DOV.PrpsRow1.Funds.Funds'] = dataframe['P3.DOV.PrpsRow1.Funds.Funds'].astype(
+    'int16')
+# relation to applicant of purpose of visit 01: string -> categorical
+dataframe['P3.DOV.cntcts_Row1.RelationshipToMe.RelationshipToMe'] = dataframe['P3.DOV.cntcts_Row1.RelationshipToMe.RelationshipToMe'].astype(
+    'string')
+# relation to applicant of purpose of visit 02: string -> categorical
+dataframe['P3.cntcts_Row2.Relationship.RelationshipToMe'] = dataframe['P3.cntcts_Row2.Relationship.RelationshipToMe'].astype(
+    'string')
+# higher education: bool -> binary
+dataframe['P3.Edu.EduIndicator'] = dataframe['P3.Edu.EduIndicator'].apply(
+    lambda x: True if x == 'Y' else False)
+# higher education period: none -> string year -> int days
+dataframe = fillna_datetime(
+    dataframe=dataframe, col_base_name='P3.Edu.Edu_Row1')
+dataframe = aggregate_datetime(dataframe=dataframe, col_base_name='P3.Edu.Edu_Row1',
+                               new_col_name='Period', reference_date=None,
+                               current_date=None)
+
+# TODO: add a method that determines the type of study [too general, zero priority].
+#   at the moment, we delete this too. See #1
+# field of study: string -> extract main keyword: eng, medical, other -> categorical
+# dataframe['P3.Edu.Edu_Row1.FieldOfStudy'] = dataframe['P3.Edu.Edu_Row1.FieldOfStudy'].astype(
+#     'string')
+
+column_dropper(dataframe=dataframe, string='P3.Edu.Edu_Row1.FieldOfStudy')
+# higher education country: string -> categorical
+dataframe['P3.Edu.Edu_Row1.Country.Country'] = dataframe['P3.Edu.Edu_Row1.Country.Country'].astype(
+    'string')
+# occupation period 01: none -> string year -> int days
+dataframe = fillna_datetime(dataframe=dataframe, col_base_name='P3.Occ.OccRow1', one_sided=True,
+                            date=dataframe['P3.Sign.C1CertificateIssueDate'])
+dataframe = aggregate_datetime(dataframe=dataframe, col_base_name='P3.Occ.OccRow1',
+                               new_col_name='Period', reference_date=None,
+                               current_date=None)
+# TODO: add verification and make sure data falls into following categories at the begining at least.
+# occupation type 01: string, employee, student, housewife, entrepreneur, etc -> categorical
+dataframe['P3.Occ.OccRow1.Occ.Occ'] = dataframe['P3.Occ.OccRow1.Occ.Occ'].astype(
+    'string')
+# occupation country: string -> categorical
+dataframe['P3.Occ.OccRow1.Country.Country'] = dataframe['P3.Occ.OccRow1.Country.Country'].astype(
+    'string')
+# occupation period 02: none -> string year -> int days
+dataframe = fillna_datetime(dataframe=dataframe, col_base_name='P3.Occ.OccRow2', one_sided=True,
+                            date=dataframe['P3.Sign.C1CertificateIssueDate'])
+dataframe = aggregate_datetime(dataframe=dataframe, col_base_name='P3.Occ.OccRow2',
+                               new_col_name='Period', reference_date=None,
+                               current_date=None)
+# occupation type 01: string, employee, student, housewife, entrepreneur, etc -> categorical
+dataframe['P3.Occ.OccRow2.Occ.Occ'] = dataframe['P3.Occ.OccRow2.Occ.Occ'].astype(
+    'string')
+# occupation country: string -> categorical
+dataframe['P3.Occ.OccRow2.Country.Country'] = dataframe['P3.Occ.OccRow2.Country.Country'].astype(
+    'string')
+# occupation period 03: none -> string year -> int days
+dataframe = fillna_datetime(dataframe=dataframe, col_base_name='P3.Occ.OccRow3', one_sided=True,
+                            date=dataframe['P3.Sign.C1CertificateIssueDate'])
+dataframe = aggregate_datetime(dataframe=dataframe, col_base_name='P3.Occ.OccRow3',
+                               new_col_name='Period', reference_date=None,
+                               current_date=None)
+# occupation type 01: string, employee, student, housewife, entrepreneur, etc -> categorical
+dataframe['P3.Occ.OccRow3.Occ.Occ'] = dataframe['P3.Occ.OccRow3.Occ.Occ'].astype(
+    'string')
+# occupation country: string -> categorical
+dataframe['P3.Occ.OccRow3.Country.Country'] = dataframe['P3.Occ.OccRow3.Country.Country'].astype(
+    'string')
+# medical details: string -> binary
+dataframe['P3.BGI.Details.MedicalDetails'] = dataframe['P3.BGI.Details.MedicalDetails'].apply(
+    lambda x: True if x is not None else False)
+# other than medical: string -> binary
+dataframe['P3.BGI.otherThanMedic'] = dataframe['P3.BGI.otherThanMedic'].apply(
+    lambda x: True if x is not None else False)
+# without authentication stay, work, etc: bool -> binary
+dataframe['P3.noAuthStay'] = dataframe['P3.noAuthStay'].apply(
+    lambda x: True if x == 'Y' else False)
+# deported or refused entry: bool -> binary
+dataframe['P3.refuseDeport'] = dataframe['P3.refuseDeport'].apply(
+    lambda x: True if x == 'Y' else False)
+# previously applied: bool -> binary
+dataframe['P3.BGI2.PrevApply'] = dataframe['P3.BGI2.PrevApply'].apply(
+    lambda x: True if x == 'Y' else False)
+# criminal record: bool -> binary
+dataframe['P3.PWrapper.criminalRec'] = dataframe['P3.PWrapper.criminalRec'].apply(
+    lambda x: True if x == 'Y' else False)
+# military record: bool -> binary
+dataframe['P3.PWrapper.Military.Choice'] = dataframe['P3.PWrapper.Military.Choice'].apply(
+    lambda x: True if x == 'Y' else False)
+# political, violent movement record: bool -> binary
+dataframe['P3.PWrapper.politicViol'] = dataframe['P3.PWrapper.politicViol'].apply(
+    lambda x: True if x == 'Y' else False)
+# witness of ill treatment: bool -> binary
+dataframe['P3.PWrapper.witnessIllTreat'] = dataframe['P3.PWrapper.witnessIllTreat'].apply(
+    lambda x: True if x == 'Y' else False)
--- utils/constant.py
+++ utils/constant.py
@@ -1,6 +1,6 @@
 from enum import Enum
 
-## DICTIONARY
+# DICTIONARY
 # dict of abbreviation used to shortening length of KEYS in XML to CSV conversion
 CANADA_5257E_KEY_ABBREVIATION = {
     'Page': 'P',
@@ -56,72 +56,76 @@ CANADA_5257E_VALUE_ABBREVIATION = {
     'BIOMETRIC ENROLMENT': 'Bio'
 }
 
-## LIST
+# LIST
 CANADA_5257E_DROP_COLUMNS = [
     'ns0:datasets.@xmlns:ns0', 'P1.Header.CRCNum', 'P1.FormVersion', 'P1.PrevSpouseAge',
     'P1.PD.UCIClientID', 'P1.PD.SecHeader.@ns0:dataNode', 'P1.PD.DOBMonth',
-    'P1.PD.DOBDay', 'P1.PD.CurrCOR.Row1.@ns0:dataNode', 'P1.PD.PrevCOR.Row1.@ns0:dataNode', 
-    'P1.PD.CWA.Row1.@ns0:dataNode', 'P1.PD.ApplicationValidatedFlag', 
-    'P2.MS.SecA.SecHeader.@ns0:dataNode', 'P2.MS.SecA.DateLastValidated.DateCalc', 
-    'P2.MS.SecA.DateLastValidated.Year', 'P2.MS.SecA.DateLastValidated.Month', 
-    'P2.MS.SecA.DateLastValidated.Day', 'P2.MS.SecA.PsprtSecHeader.@ns0:dataNode', 
+    'P1.PD.DOBDay', 'P1.PD.CurrCOR.Row1.@ns0:dataNode', 'P1.PD.PrevCOR.Row1.@ns0:dataNode',
+    'P1.PD.CWA.Row1.@ns0:dataNode', 'P1.PD.ApplicationValidatedFlag',
+    'P2.MS.SecA.SecHeader.@ns0:dataNode', 'P2.MS.SecA.DateLastValidated.DateCalc',
+    'P2.MS.SecA.DateLastValidated.Year', 'P2.MS.SecA.DateLastValidated.Month',
+    'P2.MS.SecA.DateLastValidated.Day', 'P2.MS.SecA.PsprtSecHeader.@ns0:dataNode',
     'P2.MS.SecA.Psprt.IssueYYYY', 'P2.MS.SecA.Psprt.IssueMM', 'P2.MS.SecA.Psprt.IssueDD',
     'P2.MS.SecA.Psprt.expiryYYYY', 'P2.MS.SecA.Psprt.expiryMM', 'P2.MS.SecA.Psprt.expiryDD',
     'P2.MS.SecA.Langs.languagesHeader.@ns0:dataNode', 'P2.natID.SecHeader.@ns0:dataNode',
     'P2.natID.natIDdocs.IssueDate.IssueDate', 'P2.natID.natIDdocs.ExpiryDate',
-    'P2.USCard.SecHeader.@ns0:dataNode', 'P2.USCard.SecHeader.@ns0:dataNode', 
-    'P2.USCard.usCarddocs.ExpiryDate', 'P2.USCard.usCarddocs.DocNum.DocNum', 
+    'P2.USCard.SecHeader.@ns0:dataNode', 'P2.USCard.SecHeader.@ns0:dataNode',
+    'P2.USCard.usCarddocs.ExpiryDate', 'P2.USCard.usCarddocs.DocNum.DocNum',
     'P2.CI.cntct.cntctInfoSecHeader.@ns0:dataNode', 'P2.CI.cntct.AddrRow1.POBox.POBox',
-    'P2.CI.cntct.AddrRow1.Apt.AptUnit', 'P2.CI.cntct.AddrRow1.StreetNum.StreetNum', 
+    'P2.CI.cntct.AddrRow1.Apt.AptUnit', 'P2.CI.cntct.AddrRow1.StreetNum.StreetNum',
     'P2.CI.cntct.AddrRow1.Streetname.Streetname', 'P2.CI.cntct.AddrRow2.ProvinceState.ProvinceState',
     'P2.CI.cntct.AddrRow2.PostalCode.PostalCode', 'P2.CI.cntct.AddrRow2.District',
     'P2.CI.cntct.ResiialAddrRow1.AptUnit.AptUnit', 'P2.CI.cntct.ResiialAddrRow1.StreetNum.StreetNum',
     'P2.CI.cntct.ResiialAddrRow1.StreetName.Streetname', 'P2.CI.cntct.ResiialAddrRow2.District',
     'P2.CI.cntct.ResiialAddrRow2.ProvinceState.ProvinceState', 'P2.CI.cntct.PhnNums.Phn.NumCountry',
-    'P2.CI.cntct.ResiialAddrRow2.PostalCode.PostalCode', 'P2.CI.cntct.PhnNums.Phn.ActualNum', 
-    'P2.CI.cntct.PhnNums.Phn.NANum.AreaCode', 'P2.CI.cntct.PhnNums.Phn.NANum.FirstThree', 
-    'P2.CI.cntct.PhnNums.Phn.NANum.LastFive', 'P2.CI.cntct.PhnNums.Phn.IntlNum.IntlNum', 
+    'P2.CI.cntct.ResiialAddrRow2.PostalCode.PostalCode', 'P2.CI.cntct.PhnNums.Phn.ActualNum',
+    'P2.CI.cntct.PhnNums.Phn.NANum.AreaCode', 'P2.CI.cntct.PhnNums.Phn.NANum.FirstThree',
+    'P2.CI.cntct.PhnNums.Phn.NANum.LastFive', 'P2.CI.cntct.PhnNums.Phn.IntlNum.IntlNum',
     'P2.CI.cntct.PhnNums.AltPhn.NumCountry', 'P2.CI.cntct.PhnNums.AltPhn.ActualNum',
     'P2.CI.cntct.PhnNums.AltPhn.NANum.AreaCode', 'P2.CI.cntct.PhnNums.AltPhn.NANum.FirstThree',
     'P2.CI.cntct.PhnNums.AltPhn.NANum.LastFive', 'P2.CI.cntct.PhnNums.AltPhn.IntlNum.IntlNum',
-    'P2.CI.cntct.FaxEmail.Phn.CanadaUS', 'P2.CI.cntct.FaxEmail.Phn.Other', 
+    'P2.CI.cntct.FaxEmail.Phn.CanadaUS', 'P2.CI.cntct.FaxEmail.Phn.Other',
     'P2.CI.cntct.FaxEmail.Phn.NumExt', 'P2.MS.SecA.Psprt.IssueDate.IssueDate',
-    'P2.CI.cntct.FaxEmail.Phn.NumCountry', 'P2.CI.cntct.FaxEmail.Phn.ActualNum', 
+    'P2.CI.cntct.FaxEmail.Phn.NumCountry', 'P2.CI.cntct.FaxEmail.Phn.ActualNum',
     'P2.CI.cntct.FaxEmail.Phn.NANum.AreaCode', 'P2.CI.cntct.FaxEmail.Phn.NANum.FirstThree',
     'P2.CI.cntct.FaxEmail.Phn.NANum.LastFive', 'P2.CI.cntct.FaxEmail.Phn.IntlNum.IntlNum',
-    'P2.CI.cntct.FaxEmail.Email', 'P3.SecHeader_DOV.@ns0:dataNode', 
+    'P2.CI.cntct.FaxEmail.Email', 'P3.SecHeader_DOV.@ns0:dataNode',
     'P3.DOV.cntcts_Row1.AddrInCanada.AddrInCanada', 'P3.Edu.Edu_SecHeader.@ns0:dataNode',
     'P3.Occ.SecHeader_CurrOcc.@ns0:dataNode', 'P3.BGI_SecHeader.@ns0:dataNode',
     'P3.Sign.Consent0.Choice', 'P3.Sign.hand.@ns0:dataNode', 'P3.Sign.TextField2',
-    'P3.Disclosure.@ns0:dataNode', 'P3.ReaderInfo', 'Barcodes.@ns0:dataNode', 
-    'P3.BGI2.Details.refusedDetails', 'P3.PWrapper.BGI3.details', 
+    'P3.Disclosure.@ns0:dataNode', 'P3.ReaderInfo', 'Barcodes.@ns0:dataNode',
+    'P3.BGI2.Details.refusedDetails', 'P3.PWrapper.BGI3.details',
     'P3.PWrapper.Military.militaryServiceDetails', 'P1.PD.AliasName.AliasGivenName',
     'P1.PD.AliasName.AliasFamilyName', 'P2.MS.SecA.PMFamilyName', 'P2.MS.SecA.GivenName.PMGivenName',
-    'P3.DOV.PrpsRow1.Other.Other', 'P3.DOV.cntcts_Row1.Name.Name', 'P3.cntcts_Row2.Name.Name',
+    'P3.DOV.cntcts_Row1.Name.Name', 'P3.cntcts_Row2.Name.Name', 'P1.PD.PrevCOR.Row3.Other',
     'P3.cntcts_Row2.AddrInCanada.AddrInCanada', 'P3.Edu.Edu_Row1.FromMonth', 'P3.Edu.Edu_Row1.ToMonth',
-    'P3.Edu.Edu_Row1.School', 'P3.Edu.Edu_Row1.CityTown', 'P3.Edu.Edu_Row1.ProvState', 
-    'P3.Occ.OccRow1.FromMonth', 'P3.Occ.OccRow1.ToMonth', 'P3.Occ.OccRow1.CityTown.CityTown', 
+    'P3.Edu.Edu_Row1.School', 'P3.Edu.Edu_Row1.CityTown', 'P3.Edu.Edu_Row1.ProvState',
+    'P3.Occ.OccRow1.FromMonth', 'P3.Occ.OccRow1.ToMonth', 'P3.Occ.OccRow1.CityTown.CityTown',
     'P3.Occ.OccRow1.ProvState', 'P3.Occ.OccRow2.FromMonth', 'P3.Occ.OccRow2.ToMonth',
     'P3.Occ.OccRow2.CityTown.CityTown', 'P3.Occ.OccRow2.ProvState', 'P3.Occ.OccRow3.FromMonth',
     'P3.Occ.OccRow3.ToMonth', 'P3.Occ.OccRow3.CityTown.CityTown', 'P3.Occ.OccRow3.ProvState',
-    'P1.PD.Name.FamilyName', 'P1.PD.Name.GivenName', 'P1.PD.PrevCOR.Row2.Other', 'P1.PD.PrevCOR.Row3.Other',
-    'P1.MS.SecA.FamilyName', 'P1.MS.SecA.GivenName', 'P2.MS.SecA.PrevSpouseDOB.DOBMonth', 
+    'P1.PD.Name.FamilyName', 'P1.PD.Name.GivenName', 'P1.PD.PrevCOR.Row2.Other',
+    'P1.MS.SecA.FamilyName', 'P1.MS.SecA.GivenName', 'P2.MS.SecA.PrevSpouseDOB.DOBMonth',
     'P2.MS.SecA.PrevSpouseDOB.DOBDay', 'P2.MS.SecA.Psprt.PsprtNum.PsprtNum',
-    'P2.natID.natIDdocs.DocNum.DocNum', 
-    
+    'P2.natID.natIDdocs.DocNum.DocNum', 'P2.MS.SecA.Langs.languages.lov', 'P3.Occ.OccRow1.Employer',
+    'P3.Occ.OccRow2.Employer', 'P3.Occ.OccRow3.Employer',
 ]
 
-## ENUM
+# ENUM
+
+
 class DOC_TYPES(Enum):
     """
     Contains all document types which can be used to customize ETL steps for each doc.
     Remark: Order of docs is meaningless.
     """
-    
+
     canada_5257e = 1  # application for visitor visa (temporary resident visa)
     canada_5645e = 2  # Family information
 
 # Cutoff terms for both keys and values
+
+
 class CANADA_CUTOFF_TERMS(Enum):
     """
     Dict of cut off terms for different files that is can be used with
