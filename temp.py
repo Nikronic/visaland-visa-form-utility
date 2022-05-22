7d80a887ad895a6e623471933d83c2d8e4f72680
Nikan <nikan.doosti@outlook.com>
2022-05-22 17:29:05 +0430

    add conversion of country name to numeric #10

--- temp.py
+++ temp.py
@@ -1,3 +1,5 @@
+from tracemalloc import start
+from typing import Union
 import os
 import shutil
 import logging
@@ -8,8 +10,6 @@ from utils.constant import DOC_TYPES
 from utils import functional
 from utils.preprocessor import *
 
-import dvc.api
-import mlflow
 import pandas as pd
 
 
@@ -32,115 +32,61 @@ manager = enlighten.get_manager()  # setup progress bar
 logger.info(
     '\t\t↓↓↓ Starting setting up configs: dirs, mlflow, dvc, etc ↓↓↓')
 # main path
-SRC_DIR = '/mnt/e/dataset/processed/all/'  # path to source encrypted pdf
-DST_DIR = 'raw-dataset/all/'  # path to decrypted pdf
-
-# MLFlow configs
-# data versioning config
-PATH = DST_DIR[:-1] + '.pkl'  # path to source data, e.g. data.pkl file
-REPO = '/home/nik/visaland-visa-form-utility'
-VERSION = 'v0.0.2'
-
-# log experiment configs
-MLFLOW_EXPERIMENT_NAME = 'setting up logging integrated into mlflow'
-mlflow.set_experiment(MLFLOW_EXPERIMENT_NAME)
-MLFLOW_TAGS = {
-    'stage': 'dev'  # dev, beta, production
-}
-mlflow.set_tags(MLFLOW_TAGS)
-
-logger.info('MLflow experiment name: {}'.format(MLFLOW_EXPERIMENT_NAME))
-logger.info('MLflow experiment id: {}'.format(mlflow.active_run().info.run_id))
-logger.info('MLflow data version: {}'.format(VERSION))
-logger.info('MLflow repo (root): {}'.format(REPO))
-logger.info('MLflow data source path: {}'.format(PATH))
-logger.info(
-    '\t\t↑↑↑ Finished setting up configs: dirs, mlflow, dvc, etc ↑↑↑')
+FILE_NAME = 'databank-2015-2019.csv'
+SRC_DIR = 'raw-dataset/field_data/'
+SRC_FILE = SRC_DIR + FILE_NAME
 
 # main code
-logger.info('\t\t↓↓↓ Starting data extraction ↓↓↓')
-# Canada protected PDF to machine readable for all entries and transfering other files as it is
-compose = {
-    CopyFile(mode='cf'): '.csv',
-    CopyFile(mode='cf'): '.txt',
-    MakeContentCopyProtectedMachineReadable(): '.pdf'
-}
-file_transform_compose = FileTransformCompose(transforms=compose)
-functional.process_directory(src_dir=SRC_DIR, dst_dir=DST_DIR,
-                             compose=file_transform_compose, file_pattern='*')
+logger.info('\t\t↓↓↓ Starting loading raw data ↓↓↓')
+dataframe = pd.read_csv(SRC_FILE)
+logger.info('\t\t↑↑↑ Finished loading raw data ↑↑↑')
 
-# convert PDFs to pandas dataframes
-data_iter_logger = logging.getLogger(logger.name+'.data_iter')
 
-SRC_DIR = DST_DIR[:-1]
-dataframe = pd.DataFrame()
-progress_bar = manager.counter(total=len(next(os.walk(DST_DIR), (None, [], None))[1]),
-                               desc='Ticks', unit='ticks')
-i = 0  # for progress bar
-for dirpath, dirnames, all_filenames in os.walk(SRC_DIR):
-    dataframe_entry = pd.DataFrame()
 
-    # filter all_filenames
-    filenames = all_filenames
-    if filenames:
-        files = [os.path.join(dirpath, fname) for fname in filenames]
-        # applicant form
-        in_fname = [f for f in files if '5257' in f][0]
-        df_preprocessor = CanadaDataframePreprocessor()
-        if len(in_fname) != 0:
-            dataframe_applicant = df_preprocessor.file_specific_basic_transform(
-                path=in_fname, type=DOC_TYPES.canada_5257e)
-        # applicant family info
-        in_fname = [f for f in files if '5645' in f][0]
-        if len(in_fname) != 0:
-            dataframe_family = df_preprocessor.file_specific_basic_transform(
-                path=in_fname, type=DOC_TYPES.canada_5645e)
-        # manually added labels
-        in_fname = [f for f in files if 'label' in f][0]
-        if len(in_fname) != 0:
-            dataframe_label = df_preprocessor.file_specific_basic_transform(
-                path=in_fname, type=DOC_TYPES.canada_label)
+# constans
+INDICATOR = 'Indicator'
+SUBINDICATOR = 'Subindicator Type'
+subindicator_rank = False
+SUBINDICATOR_TYPE = 'Rank' if subindicator_rank else '1-7 Best'
 
-        # final dataframe: concatenate common forms and label column wise
-        dataframe_entry = pd.concat(
-            objs=[dataframe_applicant, dataframe_family, dataframe_label],
-            axis=1, verify_integrity=True)
+years = (2015, 2017)
 
-    # concat the dataframe_entry into the main dataframe (i.e. adding rows)
-    dataframe = pd.concat(objs=[dataframe, dataframe_entry], axis=0,
-                          verify_integrity=True, ignore_index=True)
-    # logging
-    i += 1
-    data_iter_logger.info('Processed {}th data point ...'.format(i))
-    progress_bar.update()
+# drop useless columns
+columns_to_drop = ['Country ISO3', 'Indicator Id', ]
+columns_to_drop = columns_to_drop + \
+    [c for c in dataframe.columns.values if '-' in c]
+dataframe.drop(columns_to_drop, axis=1, inplace=True)
+
+# figure out start and end year index of columns names values
+start_year, end_year = [
+    str(y) for y in years] if years is not None else (None, None)
+column_years = [c for c in dataframe.columns.values if c.isnumeric()]
+start_year_index = column_years.index(
+    start_year) if start_year is not None else 0
+end_year_index = column_years.index(
+    end_year) if end_year is not None else -1
+# dataframe with desired years
+sub_column_years = column_years[start_year_index: end_year_index+1]
+columns_to_drop = [c for c in list(
+    set(column_years) - set(sub_column_years)) if c.isnumeric()]
+dataframe.drop(columns_to_drop, axis=True, inplace=True)
+
+# filter rows that only contain the provided `indicator_name` with type `rank` or `score`
+indicator_name = 'Quality of the education system, 1-7 (best)'
+dataframe = dataframe[(dataframe[INDICATOR] == indicator_name) &
+                        (dataframe[SUBINDICATOR] == SUBINDICATOR_TYPE)]
+dataframe.drop([INDICATOR, SUBINDICATOR], axis=1, inplace=True)
+
+dataframe[indicator_name + '_mean'] = dataframe.mean(axis=1, skipna=True,
+                                                     numeric_only=True)
+
+
+
+
 
 # save dataframe to disc as pickle
 logger.info('\t\t↓↓↓ Starting saving dataframe to disc ↓↓↓')
-dataset_path = DST_DIR[:-1] + '.pkl'
+dataset_path = SRC_DIR[:-1] + FILE_NAME[:-4] + '.pkl'
 dataframe.to_pickle(dataset_path)
 logger.info('Dataframe saved to path={}'.format(dataset_path))
 logger.info('\t\t↑↑↑ Finished saving dataframe to disc ↑↑↑')
-
-# get url data from DVC data storage
-data_url = dvc.api.get_url(path=PATH, repo=REPO, rev=VERSION)
-
-# read dataset from remote (local) data storage
-dataframe = pd.read_pickle(data_url)
-logger.info('\t\t↑↑↑ Finished data extraction ↑↑↑')
-
-# log data params
-logger.info('\t\t↓↓↓ Starting logging with MLFlow ↓↓↓')
-
-mlflow.log_param('data_url', data_url)
-mlflow.log_param('raw_dataset_dir', DST_DIR)
-mlflow.log_param('data_version', VERSION)
-mlflow.log_param('input_shape', dataframe.shape)
-mlflow.log_param('input_columns', dataframe.columns.values)
-mlflow.log_param('input_dtypes', dataframe.dtypes.values)
-
-logger.info('\t\t↑↑↑ Finished logging with MLFlow ↑↑↑')
-
-# Log artifacts (logs, saved files, etc)
-mlflow.log_artifacts('artifacts/')
-# delete redundant logs, files that are logged as artifact
-shutil.rmtree('artifacts')
