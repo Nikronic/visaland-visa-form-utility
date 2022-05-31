041a5e10d34f367b87ae8c3c5bbcc28cea9e6545
Nikronic <nikan.doosti@outlook.com>
2022-05-31 10:35:07 +0430

    add gitflow track and logging for dataset construction

--- import_data.py
+++ import_data.py
@@ -13,16 +13,63 @@ In simple terms, if you added new samples, changed columns or anything that shou
 from vizard_utils.constant import DOC_TYPES
 from vizard_utils import functional
 from vizard_utils.preprocessor import *
-
-import os
 import pandas as pd
 
+import os
+import shutil
+import logging
+import uuid
+import enlighten
+import mlflow
 
+
+# configure logging
+VERBOSITY = logging.DEBUG
+
+logger = logging.getLogger(__name__)
+logger.setLevel(VERBOSITY)
+# Set up root logger, and add a file handler to root logger
+if not os.path.exists('artifacts'):
+    os.makedirs('artifacts')
+    os.makedirs('artifacts/logs/dataset')
+
+log_file_name = uuid.uuid4()
+logger_handler = logging.FileHandler(filename='artifacts/logs/dataset/{}.log'.format(log_file_name),
+                                     mode='w')
+logger.addHandler(logger_handler)
+manager = enlighten.get_manager()  # setup progress bar
+
+logger.info(
+    '\t\t↓↓↓ Starting setting up configs: dirs, mlflow, dvc, etc ↓↓↓')
 # main path
 SRC_DIR = '/mnt/e/dataset/processed/all/'  # path to source encrypted pdf
 DST_DIR = 'raw-dataset/all/'  # path to decrypted pdf
 
+# MLFlow configs
+# data versioning config
+PATH = DST_DIR[:-1] + '.pkl'  # path to source data, e.g. data.pkl file
+REPO = '/home/nik/visaland-visa-form-utility'
+VERSION = 'v1.0.1'  # the version it is GOING TO BE (the version you gonna `dvc add` and `git tag`)
+# run `git tag` and use a newer version than anything with this pattern `vx.x.x` (without `-field-*`)
+
+# log experiment configs
+MLFLOW_EXPERIMENT_NAME = 'full dataset construction from raw documents to csv data tables (prior to data analysis)'
+mlflow.set_experiment(MLFLOW_EXPERIMENT_NAME)
+MLFLOW_TAGS = {
+    'stage': 'dev'  # dev, beta, production
+}
+mlflow.set_tags(MLFLOW_TAGS)
+
+logger.info('MLflow experiment name: {}'.format(MLFLOW_EXPERIMENT_NAME))
+logger.info('MLflow experiment id: {}'.format(mlflow.active_run().info.run_id))
+logger.info('MLflow data version: {}'.format(VERSION))
+logger.info('MLflow repo (root): {}'.format(REPO))
+logger.info('MLflow data source path: {}'.format(PATH))
+logger.info(
+    '\t\t↑↑↑ Finished setting up configs: dirs, mlflow, dvc, etc ↑↑↑')
+
 # main code
+logger.info('\t\t↓↓↓ Starting data extraction ↓↓↓')
 # Canada protected PDF to machine readable for all entries and transferring other files as it is
 compose = {
     CopyFile(mode='cf'): '.csv',
@@ -33,8 +80,14 @@ file_transform_compose = FileTransformCompose(transforms=compose)
 functional.process_directory(src_dir=SRC_DIR, dst_dir=DST_DIR,
                              compose=file_transform_compose, file_pattern='*')
 
+# convert PDFs to pandas dataframes
+data_iter_logger = logging.getLogger(logger.name+'.data_iter')
+
 SRC_DIR = DST_DIR[:-1]
 dataframe = pd.DataFrame()
+progress_bar = manager.counter(total=len(next(os.walk(DST_DIR), (None, [], None))[1]),
+                               desc='Ticks', unit='ticks')
+i = 0  # for progress bar
 for dirpath, dirnames, all_filenames in os.walk(SRC_DIR):
     dataframe_entry = pd.DataFrame()
 
@@ -42,7 +95,7 @@ for dirpath, dirnames, all_filenames in os.walk(SRC_DIR):
     filenames = all_filenames
     if filenames:
         files = [os.path.join(dirpath, fname) for fname in filenames]
-        # applicant info
+        # applicant form
         in_fname = [f for f in files if '5257' in f][0]
         df_preprocessor = CanadaDataframePreprocessor()
         if len(in_fname) != 0:
@@ -67,7 +120,29 @@ for dirpath, dirnames, all_filenames in os.walk(SRC_DIR):
     # concat the dataframe_entry into the main dataframe (i.e. adding rows)
     dataframe = pd.concat(objs=[dataframe, dataframe_entry], axis=0,
                           verify_integrity=True, ignore_index=True)
+    # logging
+    i += 1
+    data_iter_logger.info('Processed {}th data point ...'.format(i))
+    progress_bar.update()
 
 # save dataframe to disc as pickle
+logger.info('\t\t↓↓↓ Starting saving dataframe to disc ↓↓↓')
 dataset_path = DST_DIR[:-1] + '.pkl'
 dataframe.to_pickle(dataset_path)
+logger.info('Dataframe saved to path={}'.format(dataset_path))
+logger.info('\t\t↑↑↑ Finished saving dataframe to disc ↑↑↑')
+logger.info('\t\t↑↑↑ Finished data extraction ↑↑↑')
+
+# log data params
+logger.info('\t\t↓↓↓ Starting logging with MLFlow ↓↓↓')
+mlflow.log_param('raw_dataset_dir', DST_DIR)
+mlflow.log_param('data_version', VERSION)
+mlflow.log_param('input_shape', dataframe.shape)
+mlflow.log_param('input_columns', dataframe.columns.values)
+mlflow.log_param('input_dtypes', dataframe.dtypes.values)
+logger.info('\t\t↑↑↑ Finished logging with MLFlow ↑↑↑')
+
+# Log artifacts (logs, saved files, etc)
+mlflow.log_artifacts('artifacts/')
+# delete redundant logs, files that are logged as artifact
+shutil.rmtree('artifacts')
