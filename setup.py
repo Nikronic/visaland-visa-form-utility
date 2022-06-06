4077c9eba3482fc26ece94336af05a540e477b24
Nikronic <nikan.doosti@outlook.com>
2022-06-06 16:50:07 +0430

    change main package name to `vizard` with `vizard_*` as subpackages

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(name='vizard_utils', version='0.1.0', packages=find_packages())
+setup(name='vizard', version='0.1.0-alpha', packages=find_packages())
