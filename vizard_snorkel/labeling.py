ebb5d5aff7e6ce48b373dac02125ac4790cc1332
Nikronic <nikan.doosti@outlook.com>
2022-06-08 15:58:07 +0430

    remove redundent check

--- vizard_snorkel/labeling.py
+++ vizard_snorkel/labeling.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 # define the label mappings
 ABSTAIN = -1
-REJ = 0  # TODO: cant be 1 so it matches our dataframe already?
+REJ = 0  # TODO: cant be 2 so it matches our dataframe already?
 ACC = 1
 
 
@@ -12,8 +12,6 @@ ACC = 1
 # convert weak accept to accept (=='w-acc' -> 'acc')
 @labeling_function()
 def lf_weak_accept(x: pd.Series) -> int:
-    # only unlabeled data is allowed here
-    assert (x['VisaResult'] != 'acc') | (x['VisaResult'] != 'rej')
     if x['VisaResult'] == 'w-acc':    # 3 == weak acc
         return ACC
     else:
@@ -22,8 +20,6 @@ def lf_weak_accept(x: pd.Series) -> int:
 # convert weak reject to reject (=='w-rej' -> 'rej')
 @labeling_function()
 def lf_weak_reject(x: pd.Series) -> int:
-    # only unlabeled data is allowed here
-    assert (x['VisaResult'] != 'acc') | (x['VisaResult'] != 'rej')
     if x['VisaResult'] == 'w-rej':  # 4 == weak rej
         return REJ
     else:
@@ -32,8 +28,9 @@ def lf_weak_reject(x: pd.Series) -> int:
 # convert no idea to reject (=='no idea' -> 'rej')
 @labeling_function()
 def lf_no_idea(x: pd.Series) -> int:
-    # only unlabeled data is allowed here
-    assert (x['VisaResult'] != 'acc') | (x['VisaResult'] != 'rej')
+    """
+    Ms. S's suggestion was that if she can't remember, then it's probably a `rej` (=rejected) case
+    """
     if x['VisaResult'] == 'no idea':  # 5 == no idea
         return REJ
     else:
