6d5c4a1e20959257b4a8cd83b84571c720e0c352
Nikronic <nikan.doosti@outlook.com>
2022-06-22 15:13:55 +0430

    bump vizard from `0.1.0-alpha` to `0.2.0-alpha`

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
-setup(name='vizard', version='0.1.0-alpha', packages=find_packages(),
+setup(name='vizard', version='0.2.0-alpha', packages=find_packages(),
       description='Vizard: Visa chance predictor, powered by AI!',
       author='Nikan Doosti',
       author_email='nikan.doosti@outlook.com',
