hotfix - wrong order when multiple TFs are provided

snorkel-0.9.8/snorkel/augmentation/utils.py
@@ -36,10 +36,10 @@ def preview_tfs(dataframe: pd.DataFrame, tfs: List[TransformationFunction],
     samples = dataframe.sample(n_samples)
     samples_augmented = tf_applier.apply(samples)
     for i in range(len(samples)):
-        for tf in tfs:
+        for j, tf in enumerate(tfs):
             column_name = tf._resources['column']
             result[f'sample_{i}_{tf.name}'] = [
-                samples.iloc[i, :][column_name], samples_augmented.iloc[i, :][column_name]]
+                samples.iloc[i, :][column_name], samples_augmented.iloc[i*len(tfs)+j, :][column_name]]
 
     # proper column name
     result = pd.DataFrame.from_dict(