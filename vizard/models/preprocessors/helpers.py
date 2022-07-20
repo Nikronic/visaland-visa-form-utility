46b02b314ece14cc2b68e70722775cc9fba66d70
Nikronic <nikan.doosti@outlook.com>
2022-07-20 10:49:23 +0430

    add parsing `OneHotEncoder`

--- vizard/models/preprocessors/helpers.py
+++ vizard/models/preprocessors/helpers.py
@@ -3,7 +3,8 @@ import numpy as np
 import pandas as pd
 # ours: models
 from vizard.models.preprocessors import ColumnTransformer
-
+from vizard.models.preprocessors import OneHotEncoder
+from vizard.utils.helpers import loggingdecorator
 # helpers
 from typing import List, Union
 import logging
@@ -13,16 +14,26 @@ import logging
 logger = logging.getLogger(__name__)
 
 
+@loggingdecorator(logger.name+'.func', level=logging.DEBUG, output=False, input=False)
 def preview_column_transformer(column_transformer: ColumnTransformer,
                                original: np.ndarray,
                                transformed: np.ndarray,
-                               columns: List[str],
-                               random_state: Union[int, np.random.Generator] = np.random.default_rng(),
+                               df: pd.DataFrame,
+                               random_state: Union[int, np.random.Generator] = np.random.default_rng(
+                               ),
                                **kwargs) -> pd.DataFrame:
     """Preview transformed data next to original one obtained via ``ColumnTransformer``
 
-    TODO:
-        - add support for one hot encoding
+    When the transformation is not :class:`sklearn.preprocessing.OneHotEncoder`,
+    the transformed data is previewed next to the original data in a pandas dataframe.
+
+    But when the transformation is :class:`sklearn.preprocessing.OneHotEncoder`,
+    this is no longer clean or informative in seeing only 0s and 1s. So, I just 
+    skip previewing the transformed data entirely but report following information:
+
+        - The number of columns affected by transformation
+        - The number of unique values in all of affected columns
+        - The number of newly produced columns
 
     Args:
         column_transformer (ColumnTransformer): An instance
@@ -31,15 +42,16 @@ def preview_column_transformer(column_transformer: ColumnTransformer,
             Same shape as ``transformed``
         transformed (np.ndarray): Transformed data as a :class:`numpy.ndarray`.
             Same shape as ``original``
-        columns (List[str]): List of column names in original dataframe that
-            ``original`` and ``transformed`` are extracted from
+        df (pd.DataFrame): A dataframe that hosts the ``original`` and ``transformed``
+            data. Used to extract column names and unique values for logging
+            information about the transformations done
         random_state (Union[int, np.random.Generator], optional): A seed value or
             instance of  :class:`numpy.random.Generator` for sampling. Defaults to
             :func:`numpy.random.default_rng()`.
         **kwargs: Additional arguments as follows:
 
             * ``n_samples`` (int): Number of samples to draw. Defaults to 1.
-    
+
     Raises:
         ValueError: If ``original`` and ``transformed`` are not of the same shape
 
@@ -53,10 +65,6 @@ def preview_column_transformer(column_transformer: ColumnTransformer,
 
     # just aliases for shorter lines
     ct = column_transformer
-    
-    # verify shapes
-    if original.shape != transformed.shape:
-        raise ValueError('original and transformed must have the same shape')
 
     # set rng
     if isinstance(random_state, int):
@@ -68,36 +76,51 @@ def preview_column_transformer(column_transformer: ColumnTransformer,
                                          replace=False)
     sample_indices = sample_indices.reshape(-1, 1)  # to broadcast properly
 
-    # loop through each transform (over subset of columns) and preview it    
+    # loop through each transform (over subset of columns) and preview it
     for idx, k in enumerate(ct.output_indices_):
         # 'remainder' is not transformed, so end of the loop
         if k == 'remainder':
             return None
+
         # get indices of the transformed columns
         transformed_columns_indices = ct.output_indices_[k]
         # get indices of the original columns
         columns_indices = ct._columns[idx]
         # get names of the original columns
-        columns_indices_names = columns[columns_indices]
-        # compare the values of the transformed and the original columns
-        original_sample = original[sample_indices, columns_indices]
-        transformed_sample = transformed[sample_indices, transformed_columns_indices]
-        # fix shapes to be 2d
-        original_sample = original_sample.reshape(sample_indices.shape[0],
-                                                  columns_indices.__len__())
-        transformed_sample = transformed_sample.reshape(sample_indices.shape[0],
-                                                  columns_indices.__len__())
-        # create a dataframe with the original and transformed columns side by side
-        sample = np.empty(
-            shape=(original_sample.shape[0], original_sample.shape[1] * 2))
-        sample[:, ::2] = original_sample
-        sample[:, 1::2] = transformed_sample
-        preview_cols: List[str] = []
-        [preview_cols.extend([f'{c}_og', f'{c}_tf'])  # type: ignore
-         for c in columns_indices_names]
-        preview_df = pd.DataFrame(sample, columns=preview_cols)
-        # yield the previews
-        if n_samples == 1:  # just better visuals for single sample
-            yield preview_df.T
-        else:
-            yield preview_df
+        columns_indices_names = df.columns.values[columns_indices]
+
+        # preview transformed and original data side by side
+        if not isinstance(ct.transformers[idx][1], OneHotEncoder):
+
+            # compare the values of the transformed and the original columns
+            original_sample = original[sample_indices, columns_indices]
+            transformed_sample = transformed[sample_indices,
+                                             transformed_columns_indices]
+            # fix shapes to be 2d
+            original_sample = original_sample.reshape(sample_indices.shape[0],
+                                                      columns_indices.__len__())
+            transformed_sample = transformed_sample.reshape(sample_indices.shape[0],
+                                                            columns_indices.__len__())
+            # create a dataframe with the original and transformed columns side by side
+            sample = np.empty(
+                shape=(original_sample.shape[0], original_sample.shape[1] * 2))
+            sample[:, ::2] = original_sample
+            sample[:, 1::2] = transformed_sample
+            preview_cols: List[str] = []
+            [preview_cols.extend([f'{c}_og', f'{c}_tf'])  # type: ignore
+             for c in columns_indices_names]
+            preview_df = pd.DataFrame(sample, columns=preview_cols)
+            # yield the previews
+            if n_samples == 1:
+                # just better visuals for single sample
+                yield preview_df.T
+            else:
+                yield preview_df
+
+        # show info about onehot encoder changes
+        elif isinstance(ct.transformers[idx][1], OneHotEncoder):
+            count_uniques = df.iloc[:, columns_indices].nunique().sum()
+            logger.info(f'{len(columns_indices)} columns are affected.')
+            logger.info(f'{count_uniques} unique values exist.'
+                        f'it is expected to have {count_uniques - len(columns_indices)} new columns '
+                        f'and {transformed.shape[1] - original.shape[1]} columns are newly produced.')
