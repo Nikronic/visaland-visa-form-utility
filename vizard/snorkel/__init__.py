eb507d0cfb94230ff5244906e2e509c0aa33c7d0
Nikronic <nikan.doosti@outlook.com>
2022-07-27 13:30:19 +0430

    handle `LabelModel` args as json config

--- vizard/snorkel/__init__.py
+++ vizard/snorkel/__init__.py
@@ -21,11 +21,33 @@ from . import augmentation
 from . import labeling
 from . import modeling
 from . import slicing
-
-
 # helpers
 import logging
+import pathlib
 
 
 # set logger
 logger = logging.getLogger(__name__)
+
+
+# path to all config/db files
+parent_dir = pathlib.Path(__file__).parent
+DATA_DIR = parent_dir / 'data'
+
+
+LABEL_MODEL_CONFIGS = DATA_DIR / 'label_model_configs.json'
+"""Path to the json file containing the configs for the LabelModel
+
+In the highest level of configs, there are keys that contain the name of method
+or property of a `Callable` object. These needs to be passed to the :meth:`parse`
+method of the :class:`vizard.configs.core.JsonConfigHandler` class.
+
+    >>> from vizard.configs import JsonConfigHandler
+    >>> from vizard.labeling.model import LabelModel
+    >>> configs = JsonConfigHandler.parse(LABEL_MODEL_CONFIGS, 'LabelModel')
+    >>> configs['method_fit']['n_epochs']
+    100
+    >>> configs['method_init']['cardinality']
+    2
+
+"""
