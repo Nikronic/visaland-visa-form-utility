50350d0c737a80cf5722eb6419eaf292ae3e15bf
Nikronic <nikan.doosti@outlook.com>
2022-09-05 11:32:24 +0430

    update `flaml` to `v1.0.11`

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ if __name__ == '__main__':
         VERSION = 'v1.2.3-dev'  # use the latest EDA version (i.e. `vx.x.x-dev`)
 
         # log experiment configs
-        MLFLOW_EXPERIMENT_NAME = f'refactor - full pipelines - {VIZARD_VERSION}'
+        MLFLOW_EXPERIMENT_NAME = f'full pipelines - {VIZARD_VERSION}'
         mlflow.set_experiment(MLFLOW_EXPERIMENT_NAME)
         mlflow.start_run()
 
