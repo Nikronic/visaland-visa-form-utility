574b9177c2ed8b8e0c72997e648d984893c086dc
Nikronic <nikan.doosti@outlook.com>
2022-09-12 11:05:15 +0430

    add doc for versioning global vars

--- vizard/version.py
+++ vizard/version.py
@@ -7,4 +7,14 @@ _MINOR = '9'
 _REVISION = '2-alpha'
 
 VERSION_SHORT = f'{_MAJOR}.{_MINOR}'
+"""Version of the package as ``major.minor``
+
+Suggested usage for stable production where breaking updates
+is not so common.
+"""
 VERSION = f'{_MAJOR}.{_MINOR}.{_REVISION}'
+"""Version of the package as ``major.minor.revision.stage.version``
+
+Suggested usage for dev, alpha and beta where breaking updates
+are frequent.
+"""
