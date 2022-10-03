1f215e6d1f13af27c144c88b730e384c8f1833e2
Nikronic <nikan.doosti@outlook.com>
2022-10-03 11:05:57 +0330

    fix using bad defaults/types for API payload

--- vizard/api/models.py
+++ vizard/api/models.py
@@ -31,7 +31,7 @@ class PredictionResponse(BaseModel):
 class Payload(BaseModel):
     # TODO: make sure default values are in correct type and value
     alias_name_indicator: bool = False
-    sex: str
+    sex: str = 'Female'  # FIXME: remove after debugging
 
     current_country_of_residence_country: str = 'iran'
     current_country_of_residence_status: str = 'citizen'
@@ -47,13 +47,13 @@ class Payload(BaseModel):
     purpose_of_visit: str = 'tourism'
     funds: float = 8000.
     contact_relation_to_me: str = 'hotel'
-    contact_relation_to_me2: str = 'hotel'
+    contact_relation_to_me2: str = 'ukn'
 
     education_indicator: bool = False
-    education_field_of_study: str = 'OTHER'
-    education_country: str = 'iran'
+    education_field_of_study: str = 'unedu'
+    education_country: str = 'Unknown'
     
-    occupation_title1: str = 'employee'
+    occupation_title1: str = 'OTHER'
     occupation_country1: str = 'iran'
     occupation_title2: str = 'OTHER'
     occupation_country2: str = 'iran'
@@ -64,7 +64,7 @@ class Payload(BaseModel):
     refused_entry_or_deport: bool = False
     previous_apply: bool = False
 
-    date_of_birth: float
+    date_of_birth: float = 25. * 365.  # FIXME: remove after debugging
 
     previous_country_of_residency_period2: float = 0
     previous_country_of_residency_period3: float = 0
@@ -74,12 +74,12 @@ class Payload(BaseModel):
     marriage_period: float = 0.
     previous_marriage_period: float = 0.
 
-    passport_expiry_date_remaining: float = 3.  # years?
+    passport_expiry_date_remaining: float = 3. * 365.  # years
     how_long_stay_period: float = 30.  # days
 
     education_period: float = 0.
 
-    occupation_period: float
+    occupation_period: float = 4. * 365.  # FIXME: remove after debugging
     occupation_period2: float = 0.
     occupation_period3: float = 0.
 
@@ -87,46 +87,46 @@ class Payload(BaseModel):
     mother_marital_status: str = 'married'
     father_marital_status: str = 'married'
 
-    child_marital_status0: str = 'OTHER'
-    child_relation0: str = 'OTHER'
-    child_marital_status1: str = 'OTHER'
-    child_relation1: str = 'OTHER'
-    child_marital_status2: str = 'OTHER'
-    child_relation2: str = 'OTHER'
-    child_marital_status3: str = 'OTHER'
-    child_relation3: str = 'OTHER'
+    child_marital_status0: str = 'unknown'
+    child_relation0: str = 'other'
+    child_marital_status1: str = 'unknown'
+    child_relation1: str = 'other'
+    child_marital_status2: str = 'unknown'
+    child_relation2: str = 'other'
+    child_marital_status3: str = 'unknown'
+    child_relation3: str = 'other'
 
-    sibling_marital_status0: str = 'OTHER'
-    sibling_relation0: str = 'OTHER'
-    sibling_marital_status1: str = 'OTHER'
-    sibling_relation1: str = 'OTHER'
-    sibling_marital_status2: str = 'OTHER'
-    sibling_relation2: str = 'OTHER'
-    sibling_marital_status3: str = 'OTHER'
-    sibling_relation3: str = 'OTHER'
-    sibling_marital_status4: str = 'OTHER'
-    sibling_relation4: str = 'OTHER'
-    sibling_marital_status5: str = 'OTHER'
-    sibling_relation5: str = 'OTHER'
-    sibling_marital_status6: str = 'OTHER'
-    sibling_relation6: str = 'OTHER'
+    sibling_marital_status0: str = 'unknown'
+    sibling_relation0: str = 'other'
+    sibling_marital_status1: str = 'unknown'
+    sibling_relation1: str = 'other'
+    sibling_marital_status2: str = 'unknown'
+    sibling_relation2: str = 'other'
+    sibling_marital_status3: str = 'unknown'
+    sibling_relation3: str = 'other'
+    sibling_marital_status4: str = 'unknown'
+    sibling_relation4: str = 'other'
+    sibling_marital_status5: str = 'unknown'
+    sibling_relation5: str = 'other'
+    sibling_marital_status6: str = 'unknown'
+    sibling_relation6: str = 'other'
 
     spouse_date_of_birth: float = 0.
-    mother_date_of_birth: float
-    father_date_of_birth: float 
+    mother_date_of_birth: float = 50. * 365.  # FIXME: remove after debugging
+    father_date_of_birth: float = 53. * 365.  # FIXME: remove after debugging
     
-    child_date_of_birth0: float = 0
-    child_date_of_birth1: float = 0
-    child_date_of_birth2: float = 0
-    child_date_of_birth3: float = 0
+    child_date_of_birth0: float = 0.
+    child_date_of_birth1: float = 0.
+    child_date_of_birth2: float = 0.
+    child_date_of_birth3: float = 0.
 
-    sibling_date_of_birth0: float = 0
-    sibling_date_of_birth1: float = 0
-    sibling_date_of_birth2: float = 0
-    sibling_date_of_birth3: float = 0
-    sibling_date_of_birth4: float = 0
-    sibling_date_of_birth5: float = 0
-    sibling_date_of_birth6: float = 0
+    sibling_date_of_birth0: float = 0.
+    sibling_date_of_birth1: float = 0.
+    sibling_date_of_birth2: float = 0.
+    sibling_date_of_birth3: float = 0.
+    sibling_date_of_birth4: float = 0.
+    sibling_date_of_birth5: float = 0.
+    sibling_date_of_birth6: float = 0.
 
     previous_country_of_residence_count: int = 0
 
@@ -135,7 +135,7 @@ class Payload(BaseModel):
 
     child_accompany: int = 0
     parent_accompany: int = 0
-    spouse_accompany: bool = False
+    spouse_accompany: int = 0
     sibling_accompany: int = 0
 
     child_count: int = 0
