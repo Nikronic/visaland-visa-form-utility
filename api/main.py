d76bb71d83879c81274019f280475cb010f41ea0
Nikronic <nikan.doosti@outlook.com>
2023-07-06 11:02:35 +0330

    add API for grouped SHAP values

--- api/main.py
+++ api/main.py
@@ -12,7 +12,9 @@ from vizard.data.constant import (
     ChildRelation,
     EducationFieldOfStudy,
     OccupationTitle,
-    SiblingRelation
+    SiblingRelation,
+    FeatureCategories,
+    FEATURE_CATEGORY_TO_FEATURE_NAME_MAP
 )
 from vizard.models import preprocessors
 from vizard.models import trainers
@@ -1073,6 +1075,158 @@ async def xai(features: api_models.Payload, k: int = 5):
     return {
         'xai_overall_score': xai_overall_score,
         'xai_top_k': xai_top_k
+    }
+
+@app.post('/grouped_xai', response_model=api_models.XaiAggregatedGroupResponse)
+async def grouped_xai(features: api_models.Payload):
+    # TODO: Some caching can be done here:
+    # 1) `sample` is shared between all `xai` methods
+    # 2) `FEATURE_CATEGORY_TO_FEATURE_NAME_MAP` can be indexed (see
+    #    `aggregate_shap_values` method)
+
+    # validate sample
+    sample = _xai(
+        alias_name_indicator=features.alias_name_indicator,
+        sex=features.sex,
+
+        current_country_of_residence_country=features.current_country_of_residence_country,
+        current_country_of_residence_status=features.current_country_of_residence_status,
+        previous_country_of_residence_country2=features.previous_country_of_residence_country2,
+        previous_country_of_residence_country3=features.previous_country_of_residence_country3,
+
+        same_as_country_of_residence_indicator=features.same_as_country_of_residence_indicator,
+        country_where_applying_country=features.country_where_applying_country,
+        country_where_applying_status=features.country_where_applying_status,
+
+        previous_marriage_indicator=features.previous_marriage_indicator,
+
+        purpose_of_visit=features.purpose_of_visit,
+        funds=features.funds,
+        contact_relation_to_me=features.contact_relation_to_me,
+        contact_relation_to_me2=features.contact_relation_to_me2,
+
+        education_indicator=features.education_indicator,
+        education_field_of_study=features.education_field_of_study,
+        education_country=features.education_country,
+
+        occupation_title1=features.occupation_title1,
+        occupation_country1=features.occupation_country1,
+        occupation_title2=features.occupation_title2,
+        occupation_country2=features.occupation_country2,
+        occupation_title3=features.occupation_title3,
+        occupation_country3=features.occupation_country3,
+
+        no_authorized_stay=features.no_authorized_stay,
+        refused_entry_or_deport=features.refused_entry_or_deport,
+        previous_apply=features.previous_apply,
+
+        date_of_birth=features.date_of_birth,
+
+        previous_country_of_residency_period2=features.previous_country_of_residency_period2,
+        previous_country_of_residency_period3=features.previous_country_of_residency_period3,
+
+        country_where_applying_period=features.country_where_applying_period,  # days
+
+        marriage_period=features.marriage_period,
+        previous_marriage_period=features.previous_marriage_period,
+
+        passport_expiry_date_remaining=features.passport_expiry_date_remaining,  # years?
+        how_long_stay_period=features.how_long_stay_period,  # days
+
+        education_period=features.education_period,
+
+        occupation_period=features.occupation_period,
+        occupation_period2=features.occupation_period2,
+        occupation_period3=features.occupation_period3,
+
+        applicant_marital_status=features.applicant_marital_status,
+        mother_marital_status=features.mother_marital_status,
+        father_marital_status=features.father_marital_status,
+
+        child_marital_status0=features.child_marital_status0,
+        child_relation0=features.child_relation0,
+        child_marital_status1=features.child_marital_status1,
+        child_relation1=features.child_relation1,
+        child_marital_status2=features.child_marital_status2,
+        child_relation2=features.child_relation2,
+        child_marital_status3=features.child_marital_status3,
+        child_relation3=features.child_relation3,
+
+        sibling_marital_status0=features.sibling_marital_status0,
+        sibling_relation0=features.sibling_relation0,
+        sibling_marital_status1=features.sibling_marital_status1,
+        sibling_relation1=features.sibling_relation1,
+        sibling_marital_status2=features.sibling_marital_status2,
+        sibling_relation2=features.sibling_relation2,
+        sibling_marital_status3=features.sibling_marital_status3,
+        sibling_relation3=features.sibling_relation3,
+        sibling_marital_status4=features.sibling_marital_status4,
+        sibling_relation4=features.sibling_relation4,
+        sibling_marital_status5=features.sibling_marital_status5,
+        sibling_relation5=features.sibling_relation5,
+        sibling_marital_status6=features.sibling_marital_status6,
+        sibling_relation6=features.sibling_relation6,
+
+        spouse_date_of_birth=features.spouse_date_of_birth,
+        mother_date_of_birth=features.mother_date_of_birth,
+        father_date_of_birth=features.father_date_of_birth,
+
+        child_date_of_birth0=features.child_date_of_birth0,
+        child_date_of_birth1=features.child_date_of_birth1,
+        child_date_of_birth2=features.child_date_of_birth2,
+        child_date_of_birth3=features.child_date_of_birth3,
+
+        sibling_date_of_birth0=features.sibling_date_of_birth0,
+        sibling_date_of_birth1=features.sibling_date_of_birth1,
+        sibling_date_of_birth2=features.sibling_date_of_birth2,
+        sibling_date_of_birth3=features.sibling_date_of_birth3,
+        sibling_date_of_birth4=features.sibling_date_of_birth4,
+        sibling_date_of_birth5=features.sibling_date_of_birth5,
+        sibling_date_of_birth6=features.sibling_date_of_birth6,
+
+        previous_country_of_residence_count=features.previous_country_of_residence_count,
+
+        sibling_foreigner_count=features.sibling_foreigner_count,
+        child_mother_father_spouse_foreigner_count=features.child_mother_father_spouse_foreigner_count,
+
+        child_accompany=features.child_accompany,
+        parent_accompany=features.parent_accompany,
+        spouse_accompany=features.spouse_accompany,
+        sibling_accompany=features.sibling_accompany,
+
+        child_count=features.child_count,
+        sibling_count=features.sibling_count,
+
+        long_distance_child_sibling_count=features.long_distance_child_sibling_count,
+        foreign_living_child_sibling_count=features.foreign_living_child_sibling_count,
+    )
+
+    # compute aggregated SHAP values for the sample
+    aggregated_shap_values = flaml_tree_explainer.aggregate_shap_values(
+        sample=sample,
+        feature_category_to_feature_name=FEATURE_CATEGORY_TO_FEATURE_NAME_MAP,
+    )
+
+    # replace categories Enum items with their names
+    for key in list(aggregated_shap_values.keys()):
+        aggregated_shap_values[key.name] = aggregated_shap_values.pop(key)
+
+    return {
+        'aggregated_shap_values': aggregated_shap_values,
+    }
+
+
+@app.get(
+        '/const/xai_feature_categories_types',
+        response_model=api_models.XaiFeatureCategoriesResponse)
+async def get_xai_feature_categories_types():
+    """Returns a list of names of XAI categories
+
+    Note:
+        See :class:`vizard.api.models.XaiFeatureCategoriesResponse` for more info.
+    """
+    return {
+        'xai_feature_categories_types': FeatureCategories.get_member_names()
     }
 
 
