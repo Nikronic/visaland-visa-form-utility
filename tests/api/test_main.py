8521f017ca36f18186944e28953d4c5da8b531aa
Nikan <nikan.doosti@outlook.com>
2023-11-08 11:02:57 +0330

    add a basic test for demo purposes
    Please also follow the correct structure and don't put all the tests in the same directory, oviously.

---
+++ tests/api/test_main.py
@@ -0,0 +1,20 @@
+# core
+from starlette.testclient import TestClient
+from fastapi import status
+# ours: API
+from api.main import app as vizard_api
+
+
+# the test client for the all tests
+client = TestClient(vizard_api)
+
+
+def test_read_main():
+    response = client.get('/')
+
+    expected_response = {
+        'detail': 'Not Found'
+    }
+
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.json() == expected_response
