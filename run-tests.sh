09a73b5c6beb00bf2f8064605a428f28e47b4d74
Nikan <nikan.doosti@outlook.com>
2023-11-08 11:05:43 +0330

    add a bash script for running tests
    Note that for deterministic behavior, it is better to use the same `run_id` unless there is plan to change underlying model which might cause change the way APIs respond.
    
    We had such issue when AutoML suggested `extra_tree` instead of `lgbm` and it broke all the APIs.

---
+++ run-tests.sh
@@ -0,0 +1,8 @@
+pytest \
+    --experiment_name "pytest" \
+    --verb "debug" \
+    --run_id "ef6d9606e8b64e7eb775ebe79a1b374c" \
+    --bind "0.0.0.0" \
+    --mlflow_port "5000" \
+    --gunicorn_port "8000" \
+    --workers "1"
