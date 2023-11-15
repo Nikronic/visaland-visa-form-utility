c9e7d873418ae6c4a2c32a387625f9ae244051eb
aliinreallife <aliinreallifee@gmail.com>
2023-11-15 11:17:40 +0330

    reword on code to be more parametric

--- vizard/seduce/functional.py
+++ vizard/seduce/functional.py
@@ -1,35 +1,40 @@
 import math
 import matplotlib.pyplot as plt
-import numpy as np
 
-def adjusted_sigmoid(x):
+def sigmoid(x,scaler=16):
+    return 1 / (1 + math.exp(-scaler*x))
+                
+def adjusted_sigmoid(x,adjusted_min,adjusted_max):
     # Check input
-    if (0.37<=x<=0.67):
+    if (adjusted_min<x<adjusted_max):
+
     # Shift and scale the input to fit the sigmoid function
-
+        adjusted_mean = (adjusted_min+adjusted_max)/2
         # Sigmoid function
-        x = x - 0.52 
+        sigmoid_input = x - adjusted_mean
+        a = (sigmoid(adjusted_max-adjusted_mean,scaler) - sigmoid(adjusted_min-adjusted_mean,scaler))/(adjusted_max-adjusted_min)
+        b = adjusted_min - sigmoid(adjusted_min-adjusted_mean,scaler)/a
 
-        #a = 2.77 = (adjusted_sigmoid(0.67) - adjusted_sigmoid(0.37)) / (0.67-0.37)
-        #b = 0.34 = 0.37 - adjusted_sigmoid(0.37)/a
-        sigmoid = (((1 / (1 + math.exp(-16*x))/2.7788486900405176666666666666667))+0.340069368371147392)
-        # sigmoid = 1 / (1 + math.exp(-16*x))
+        output = (sigmoid(sigmoid_input,scaler)/a)+b
 
-        return sigmoid 
+
+        return output
     else:
         return x
 
-def adjusted_sigmoid2(x):
-    return x
+
+adjusted_max = 0.65
+adjusted_min = 0.35
+
+scaler  = 32
 
 
-
-print(adjusted_sigmoid(0.37))
+print(adjusted_sigmoid(0.45,adjusted_min,adjusted_max))
 
 chances = [i/100 for i in range(101)]
 
-sigmoid_values = [adjusted_sigmoid(chance) for chance in chances]
-sigmoid_values2 = [adjusted_sigmoid2(chance) for chance in chances]
+sigmoid_values = [adjusted_sigmoid(chance,adjusted_min,adjusted_max) for chance in chances]
+sigmoid_values2 = [chance for chance in chances]
 
 # Plot the adjusted sigmoid function
 plt.plot(chances, sigmoid_values,label="adjusted")
@@ -37,8 +42,8 @@ plt.plot(chances, sigmoid_values2,label="without change")
 plt.title('adjusted Sigmoid Function')
 plt.xlabel('Chance')
 plt.ylabel('adjusted Sigmoid Value')
-plt.axvline(x=0.37, color='red', linestyle='--', label='Vertical at x=0.37')
-plt.axvline(x=0.67, color='green', linestyle='--', label='Vertical at x=0.67')
+plt.axvline(x=adjusted_min, color='red', linestyle='--', label='Vertical at x=0.35')
+plt.axvline(x=adjusted_max, color='green', linestyle='--', label='Vertical at x=0.65')
 plt.legend()
 plt.grid(True)
 plt.show()
