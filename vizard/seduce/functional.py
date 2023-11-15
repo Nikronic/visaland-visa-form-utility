b670c0d691f40ef76ef0e66cfa13c150ad27a3d9
aliinreallife <aliinreallifee@gmail.com>
2023-11-15 12:46:47 +0330

    implement functions in a `class` called `SigmoidSeducer`

--- vizard/seduce/functional.py
+++ vizard/seduce/functional.py
@@ -1,69 +1,57 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
-def sigmoid(x, scaler):
-    return 1 / (1 + np.exp(-scaler * x))
+class SigmoidSeducer:
 
-def adjusted_sigmoid(x, adjusted_min, adjusted_max, scaler):
-    # Check input
-    mask = np.logical_and(adjusted_min < x, x < adjusted_max)
+    def __init__(self) -> None:
+        pass
 
-    # Shift and scale the input to fit the sigmoid function
-    adjusted_mean = (adjusted_min + adjusted_max) / 2
-    sigmoid_input = x - adjusted_mean
+    def __type_check(x):
+        if not isinstance(x, int) or not isinstance(x, float) or not isinstance(x, np.ndarray):
+            raise ValueError(f'Type x {type(x)} is shitty.')
 
-    a = (sigmoid(adjusted_max - adjusted_mean, scaler) - \
-         sigmoid(adjusted_min - adjusted_mean, scaler)) / (adjusted_max - adjusted_min)
-    b = adjusted_min - sigmoid(adjusted_min - adjusted_mean, scaler) / a
+    def __sigmoid(self, x, scaler):
 
-    output = np.where(mask, (sigmoid(sigmoid_input, scaler) / a) + b, x)
+        self.__type_check(x)
 
-    return output
+        return 1 / (1 + np.exp(-scaler * x))
 
-def bi_level_adjusted_sigmoid(
-        x,
-        adjusted_min,
-        adjusted_max,
-        closer_adjusted_min,
-        closer_adjusted_max,
-        scaler1,
-        scaler2):
-    mask = np.logical_and(adjusted_min < x, x < adjusted_max)
+    def __adjusted_sigmoid(self, x, adjusted_min, adjusted_max, scaler):
+        # Check input
+        self.__type_check(x)
+        mask = np.logical_and(adjusted_min < x, x < adjusted_max)
 
-    # Use NumPy's vectorized operations for better performance
-    result = np.where(np.logical_and(closer_adjusted_min < x, x < closer_adjusted_max),
-                      adjusted_sigmoid(x, closer_adjusted_min, closer_adjusted_max, scaler1),
-                      adjusted_sigmoid(x, adjusted_min, adjusted_max, scaler2))
+        # Shift and scale the input to fit the sigmoid function
+        adjusted_mean = (adjusted_min + adjusted_max) / 2
+        sigmoid_input = x - adjusted_mean
 
-    return np.where(mask, result, x)
+        a = (self.__sigmoid(adjusted_max - adjusted_mean, scaler) - \
+            self.__sigmoid(adjusted_min - adjusted_mean, scaler)) / (adjusted_max - adjusted_min)
+        b = adjusted_min - self.sigmoid(adjusted_min - adjusted_mean, scaler) / a
 
-adjusted_max = 0.65
-adjusted_min = 0.35
-scaler1 = 128
-scaler2 = scaler1 / 8
+        output = np.where(mask, (self.sigmoid(sigmoid_input, scaler) / a) + b, x)
 
-distance = adjusted_max - adjusted_min
-one_third = distance / 3
-adjusted_mean = (adjusted_min + adjusted_max) / 2
-closer_adjusted_min = adjusted_mean - one_third / 2
-closer_adjusted_max = adjusted_mean + one_third / 2
+        return output
 
-#example
-#print(bi_level_adjusted_sigmoid(0.47, adjusted_min, adjusted_max, closer_adjusted_min, closer_adjusted_max, scaler1, scaler2))
+    def bi_level_adjusted_sigmoid(
+            self,
+            x: int | float | np.ndarray,
+            adjusted_min: float,
+            adjusted_max: float,
+            closer_adjusted_min: float,
+            closer_adjusted_max: float,
+            scaler1: int | float,
+            scaler2: int | float) -> np.ndarray:
+        
+        # type check
+        self.__type_check(x)
+        
 
-chances = np.linspace(0, 1, 101)
+        mask = np.logical_and(adjusted_min < x, x < adjusted_max)
 
-sigmoid_values = bi_level_adjusted_sigmoid(chances, adjusted_min, adjusted_max, closer_adjusted_min, closer_adjusted_max, scaler1, scaler2)
+        # Use NumPy's vectorized operations for better performance
+        result = np.where(np.logical_and(closer_adjusted_min < x, x < closer_adjusted_max),
+                        self.__adjusted_sigmoid(x, closer_adjusted_min, closer_adjusted_max, scaler1),
+                        self.__adjusted_sigmoid(x, adjusted_min, adjusted_max, scaler2))
 
-# Plot 
+        return np.where(mask, result, x)
 
-# plt.plot(chances, sigmoid_values, label="adjusted")
-# plt.plot(chances, chances, label="without change")
-# plt.title('Adjusted Sigmoid Function')
-# plt.xlabel('Chance')
-# plt.ylabel('Adjusted Sigmoid Value')
-# plt.axvline(x=adjusted_min, color='red', linestyle='--')
-# plt.axvline(x=adjusted_max, color='red', linestyle='--')
-# plt.legend()
-# plt.grid(True)
-# plt.show()
