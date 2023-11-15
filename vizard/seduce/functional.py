5a07de55d0576a804808fcbd9d44316ae98ab5cd
aliinreallife <aliinreallifee@gmail.com>
2023-11-15 15:34:10 +0330

    add documentation

--- vizard/seduce/functional.py
+++ vizard/seduce/functional.py
@@ -1,57 +1,81 @@
 import numpy as np
 
-class SigmoidSeducer:
+def sigmoid(x, scaler):
+    """sigmoid function with a custome scaler 
+    Args:
+        x (int | float | np.ndarray): Input of the sigmid function
+        scaler (int): our scaler that mutiply every input of x ,new input = old input*scaler, 1 for normal sigmoid 
+    Returns:
+        int | float | np.ndarray:
+        output of the sigmoid function with our customized scaler --> f(x) = 1 / (1 + e^(-scaler*x))
 
-    def __init__(self) -> None:
-        pass
+    """
+    return 1 / (1 + np.exp(-scaler * x))
 
-    def __type_check(x):
-        if not isinstance(x, int) or not isinstance(x, float) or not isinstance(x, np.ndarray):
-            raise ValueError(f'Type x {type(x)} is shitty.')
+def adjusted_sigmoid(x, adjusted_min, adjusted_max, scaler):
+    """apply sigmoid to some parts of our function
+    Args:
+        x (int | float | np.ndarray): Input of the sigmid function
+        adjusted_min (float): start of our adjustmet inputs before that wont get any changes
+        adjusted_max (float): end of our adjustmet, inputs after that wont get any changes
+        scaler (int): our scaler that mutiply every input of x ,new input = old input*scaler, 1 for normal sigmoid 
 
-    def __sigmoid(self, x, scaler):
+    Returns:
+        int | float | np.ndarray:
+        for inputs of x, adjusted_min < x < adjusted_max gives the
+        output of the sigmoid function with our customized scaler --> f(x) = 1 / (1 + e^(-scaler*x))
+        for other inputs return them witout any change
 
-        self.__type_check(x)
+        `This is an extention of :vizard.seduce.functional:`
+    """
+    # Check input
+    mask = np.logical_and(adjusted_min < x, x < adjusted_max)
 
-        return 1 / (1 + np.exp(-scaler * x))
+    # Shift and scale the input to fit the sigmoid function
+    
+    adjusted_mean = (adjusted_min + adjusted_max) / 2
+    sigmoid_input = x - adjusted_mean
 
-    def __adjusted_sigmoid(self, x, adjusted_min, adjusted_max, scaler):
-        # Check input
-        self.__type_check(x)
-        mask = np.logical_and(adjusted_min < x, x < adjusted_max)
+    a = (sigmoid(adjusted_max - adjusted_mean, scaler) - \
+        sigmoid(adjusted_min - adjusted_mean, scaler)) / (adjusted_max - adjusted_min)
+    b = adjusted_min - sigmoid(adjusted_min - adjusted_mean, scaler) / a
 
-        # Shift and scale the input to fit the sigmoid function
-        adjusted_mean = (adjusted_min + adjusted_max) / 2
-        sigmoid_input = x - adjusted_mean
+    output = np.where(mask, (sigmoid(sigmoid_input, scaler) / a) + b, x)
 
-        a = (self.__sigmoid(adjusted_max - adjusted_mean, scaler) - \
-            self.__sigmoid(adjusted_min - adjusted_mean, scaler)) / (adjusted_max - adjusted_min)
-        b = adjusted_min - self.sigmoid(adjusted_min - adjusted_mean, scaler) / a
+    return output
 
-        output = np.where(mask, (self.sigmoid(sigmoid_input, scaler) / a) + b, x)
+def bi_level_adjusted_sigmoid(
+        self,
+        x: int | float | np.ndarray,
+        adjusted_min: float,
+        adjusted_max: float,
+        closer_adjusted_min: float,
+        closer_adjusted_max: float,
+        scaler1: int | float,
+        scaler2: int | float) -> np.ndarray:
+    """apply sigmoid to limited parts of function foucus more on closer part to middle of our adjusment
+    Args:
+        x (int | float | np.ndarray): Input of the sigmid function
+        adjusted_min (float): start of our adjustmet inputs before that wont get any changes
+        closer_adjusted_min (float): min for closer part to middle of our adjusment this part will be modifed more
+        adjusted_max (float): end of our adjustmet, inputs after that wont get any changes
+        closer_adjusted_max (float): max for closer part to middle of our adjusment this part will be modifed more
+        scaler1 (int): our scaler that mutiply every input of x ,new input = old_input*scaler, 1 for normal sigmoid, this scaler will be used in closer to middle part
+        scaler2 (int): our scaler that mutiply every input of x ,new input = old_input*scaler, 1 for normal sigmoid, this scaler will be used in farther to middle part
 
-        return output
+    Returns:
+        int | float | np.ndarray:
+        for inputs of x, closer_adjusted_min < x < closer_adjusted_max gives the
+        output of the sigmoid function with our customized scaler --> f(x) = 1 / (1 + e^(-scaler1*x))
+        for other inputs that are x, adjusted_min < x < adjusted_max gives the
+        output of the sigmoid function with our customized scaler --> f(x) = 1 / (1 + e^(-scaler2*x))
+        for any other inputs return them witout any change
+    """
+    mask = np.logical_and(adjusted_min < x, x < adjusted_max)
 
-    def bi_level_adjusted_sigmoid(
-            self,
-            x: int | float | np.ndarray,
-            adjusted_min: float,
-            adjusted_max: float,
-            closer_adjusted_min: float,
-            closer_adjusted_max: float,
-            scaler1: int | float,
-            scaler2: int | float) -> np.ndarray:
-        
-        # type check
-        self.__type_check(x)
-        
+    # Use NumPy's vectorized operations for better performance
+    result = np.where(np.logical_and(closer_adjusted_min < x, x < closer_adjusted_max),
+                    adjusted_sigmoid(x, closer_adjusted_min, closer_adjusted_max, scaler1),
+                    adjusted_sigmoid(x, adjusted_min, adjusted_max, scaler2))
 
-        mask = np.logical_and(adjusted_min < x, x < adjusted_max)
-
-        # Use NumPy's vectorized operations for better performance
-        result = np.where(np.logical_and(closer_adjusted_min < x, x < closer_adjusted_max),
-                        self.__adjusted_sigmoid(x, closer_adjusted_min, closer_adjusted_max, scaler1),
-                        self.__adjusted_sigmoid(x, adjusted_min, adjusted_max, scaler2))
-
-        return np.where(mask, result, x)
-
+    return np.where(mask, result, x)
