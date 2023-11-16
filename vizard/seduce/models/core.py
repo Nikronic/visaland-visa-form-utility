b815f8d50945bc69ff933ebab7c8f8415fa2801d
aliinreallife <aliinreallifee@gmail.com>
2023-11-16 09:29:04 +0330

    add documentation to core.py

--- vizard/seduce/models/core.py
+++ vizard/seduce/models/core.py
@@ -1,24 +1,29 @@
 import numpy as np
 from vizard.seduce import functional
 
-class SigmoidSeducer:
 
+class SigmoidSeducer:
     def __init__(self) -> None:
         pass
 
     def __type_check(x):
-        if not isinstance(x, int) or not isinstance(x, float) or not isinstance(x, np.ndarray):
-            raise ValueError(f'Type x {type(x)} is not right.')
+        if (
+            not isinstance(x, int)
+            or not isinstance(x, float)
+            or not isinstance(x, np.ndarray)
+        ):
+            raise ValueError(f"Type x {type(x)} is not right.")
 
-    def __sigmoid(self, x, scaler):
-        
-        self.__type_check(x)
-        result = functional.sigmoid(x,scaler)
-        return result
+    def sigmoid(self, x, scaler):
+        """sigmoid function with a custom scaler
+        for more information check :func:vizard.seduce.functional.sigmoid
 
-    def __adjusted_sigmoid(self, x, adjusted_min, adjusted_max, scaler):
         """
-        
+        return functional.sigmoid(x, scaler)
+
+    def adjusted_sigmoid(self, x, adjusted_min, adjusted_max, scaler):
+        """apply sigmoid to some parts of our function
+        for more information check :func:vizard.seduce.functional.adjusted_sigmoid
         """
         # Check input
         self.__type_check(x)
@@ -27,18 +32,29 @@ class SigmoidSeducer:
         return output
 
     def bi_level_adjusted_sigmoid(
-            self,
-            x: int | float | np.ndarray,
-            adjusted_min: float,
-            adjusted_max: float,
-            closer_adjusted_min: float,
-            closer_adjusted_max: float,
-            scaler1: int | float,
-            scaler2: int | float) -> np.ndarray:
-        result = functional.bi_level_adjusted_sigmoid(x,adjusted_min,adjusted_max,closer_adjusted_min,closer_adjusted_max,scaler1,scaler2)
-        
+        self,
+        x: int | float | np.ndarray,
+        adjusted_min: float,
+        adjusted_max: float,
+        closer_adjusted_min: float,
+        closer_adjusted_max: float,
+        scaler1: int | float,
+        scaler2: int | float,
+    ) -> np.ndarray:
+        """apply sigmoid to limited parts of function focus more on closer part to middle of our adjustment
+        for more information check :func:vizard.seduce.functional.bi_level_adjusted_sigmoid
+        """
+        result = functional.bi_level_adjusted_sigmoid(
+            x,
+            adjusted_min,
+            adjusted_max,
+            closer_adjusted_min,
+            closer_adjusted_max,
+            scaler1,
+            scaler2,
+        )
+
         # type check
         self.__type_check(x)
 
         return result
-
