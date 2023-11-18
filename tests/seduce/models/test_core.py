29f7a672d2c802c1326412fc9aee5a4df9a4037e
aliinreallife <aliinreallifee@gmail.com>
2023-11-18 09:53:58 +0330

    add class tests

---
+++ tests/seduce/models/test_core.py
@@ -0,0 +1,67 @@
+import numpy as np
+from vizard.seduce.models.core import SigmoidSeducer
+
+input_numpy = np.linspace(1, 100, 5) / 100
+input_float = 0.4
+adjusted_min = 0.3
+adjusted_max = 0.7
+closer_adjusted_min = 0.4
+closer_adjusted_max = 0.6
+scaler1 = 8
+scaler2 = 2
+
+zad = SigmoidSeducer()
+
+
+class TestSigmoidSeducer:
+    def test_sigmoid(self):
+        output_float = zad.sigmoid(input_float, scaler1)
+        correct_float = 0.9608342772032357
+        assert output_float == correct_float, "float sigmoid failed"
+        output_numpy: np.ndarray = zad.sigmoid(input_numpy, scaler1)
+        correct_numpy = np.array(
+            [0.50499983, 0.62597786, 0.73302015, 0.81831902, 0.88079708]
+        )
+        print(output_float)
+        assert output_numpy.all() == correct_numpy.all(), "np array sigmoid failed"
+
+    def test_adjusted_sigmoid(self):
+        output_float = zad.adjusted_sigmoid(
+            input_float, adjusted_min, adjusted_max, scaler1
+        )
+        correct_float = 0.3855638786081178
+        assert output_float == correct_float, "float adjusted sigmoid failed"
+        output_numpy: np.ndarray = zad.adjusted_sigmoid(
+            input_float, adjusted_min, adjusted_max, scaler1
+        )
+        correct_numpy = np.array([0.01, 0.2575, 0.50506645, 0.7525, 1.0])
+        assert (
+            output_numpy.all() == correct_numpy.all()
+        ), "np array adjusted sigmoid failed"
+
+    def test_bi_level_adjusted_sigmoid(self):
+        output_float = zad.bi_level_adjusted_sigmoid(
+            input_float,
+            adjusted_min,
+            adjusted_max,
+            closer_adjusted_min,
+            closer_adjusted_max,
+            scaler1,
+            scaler2,
+        )
+        correct_float = 0.39975041607715656
+        assert output_float == correct_float, "float bi level adjusted sigmoid failed"
+        output_numpy: np.ndarray = zad.bi_level_adjusted_sigmoid(
+            input_numpy,
+            adjusted_min,
+            adjusted_max,
+            closer_adjusted_min,
+            closer_adjusted_max,
+            scaler1,
+            scaler2,
+        )
+        correct_numpy = np.array([0.01, 0.2575, 0.50526316, 0.7525, 1])
+        print(output_float)
+        assert (
+            output_numpy.all() == correct_numpy.all()
+        ), "np array bi level adjusted sigmoid failed"
