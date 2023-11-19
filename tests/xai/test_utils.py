e9c40d2debcec21d8d69822e4811a7a2efb715ad
aliinreallife <aliinreallifee@gmail.com>
2023-11-19 10:31:31 +0330

    add `logical_order` test

--- tests/xai/test_utils.py
+++ tests/xai/test_utils.py
@@ -2,6 +2,8 @@ from vizard.xai import utils
 
 is_answered = ["applicant_marital_status"]
 answers = {"applicant_marital_status": 7}
+question_title = "child_average_age"
+correct_order_question = "applicant_marital_status"
 correct_answer = (
     [
         "applicant_marital_status",
@@ -29,3 +31,8 @@ correct_answer = (
 def test_logical_questions():
     check = utils.logical_questions(is_answered, answers)
     assert check == correct_answer, "test logical questions has been failed"
+
+
+def test_logical_order():
+    check = utils.logical_order(question_title)
+    assert check == correct_order_question, "test logical order has been failed"
