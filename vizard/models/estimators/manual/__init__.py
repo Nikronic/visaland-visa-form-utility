6b49110c61099e0b757ac8e2c76372b977cf8406
Nikan Doosti <nikan.doosti@outlook.com>
2023-11-19 12:33:37 +0330

    add `ParameterBuilderBase` class for manual inclusion of parameters

---
+++ vizard/models/estimators/manual/__init__.py
@@ -0,0 +1,10 @@
+"""Hosts all heuristics for integration of models constructed based on domain experts' knowledge
+
+Examples could be if-else models, frequency based statistical models, etc.
+"""
+
+# helpers
+import logging
+
+# set logger
+logger = logging.getLogger(__name__)
