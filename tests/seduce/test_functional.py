645e714e74f8f9253777e4c6b89466db16d04374
aliinreallife <aliinreallifee@gmail.com>
2023-11-20 16:54:55 +0330

    isolating tests in `test_functional`

--- tests/seduce/test_functional.py
+++ tests/seduce/test_functional.py
@@ -1,17 +1,12 @@
 import numpy as np
 from vizard.seduce import functional
 
-input_numpy = np.linspace(1, 100, 5) / 100
-input_float = 0.4
-adjusted_min = 0.3
-adjusted_max = 0.7
-closer_adjusted_min = 0.4
-closer_adjusted_max = 0.6
-scaler1 = 8
-scaler2 = 2
-
 
 def test_sigmoid():
+    input_numpy = np.linspace(1, 100, 5) / 100
+    input_float = 0.4
+    scaler1 = 8
+
     output_float = functional.sigmoid(input_float, scaler1)
     correct_float = 0.9608342772032357
     assert output_float == correct_float, "float sigmoid failed"
@@ -23,6 +18,11 @@ def test_sigmoid():
 
 
 def test_adjusted_sigmoid():
+    input_numpy = np.linspace(1, 100, 5) / 100
+    input_float = 0.4
+    adjusted_min = 0.3
+    adjusted_max = 0.7
+    scaler1 = 8
     output_float = functional.adjusted_sigmoid(
         input_float, adjusted_min, adjusted_max, scaler1
     )
@@ -32,10 +32,18 @@ def test_adjusted_sigmoid():
         input_float, adjusted_min, adjusted_max, scaler1
     )
     correct_numpy = np.array([0.01, 0.2575, 0.50506645, 0.7525, 1.0])
-    assert output_numpy.all() == correct_numpy.all(), "np array adjusted sigmoid failed"
+    assert np.isclose(output_numpy,correct_numpy).all(), "np array adjusted sigmoid failed"
 
 
 def test_bi_level_adjusted_sigmoid():
+    input_numpy = np.linspace(1, 100, 5) / 100
+    input_float = 0.4
+    adjusted_min = 0.3
+    adjusted_max = 0.7
+    closer_adjusted_min = 0.4
+    closer_adjusted_max = 0.6
+    scaler1 = 8
+    scaler2 = 2
     output_float = functional.bi_level_adjusted_sigmoid(
         input_float,
         adjusted_min,
