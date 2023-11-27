0475252fcf20bdf8aadb1fbd98b837a8e24908bb
Nikan Doosti <nikan.doosti@outlook.com>
2023-11-27 16:43:27 +0330

    enable `extend_mean` to do proportional shifting
    the new equation takes another value (on top of old definition) which allows proportional shifting. This allowed us to move values closer to the "base" state. (value of "base" is used as the shift). Thanks to this change, refinement of other responses importance values is no longer needed.

--- tests/models/estimators/manual/test_functional.py
+++ tests/models/estimators/manual/test_functional.py
@@ -5,14 +5,20 @@ from pytest import mark
 
 
 @mark.parametrize(
-    argnames=["given_percent", "given_new_value", "expected"],
+    argnames=["given_percent", "given_new_value", "given_shift", "expected"],
     argvalues=[
-        (0.0, 0.3, 0.3),  # left boundary
-        (1.0, 0.3, 1.0),  # right boundary
-        (0.1, 0.8, 0.82),  # middle points
-        (0.8, 0.1, 0.82),  # middle points
+        (0.0, 0.3, -0.5, 0.3),  # left boundary
+        (1.0, 0.3, -0.5, 0.8),  # right boundary
+        (0.1, 0.8, -0.4, 0.86),  # middle points
+        (0.8, 0.1, -0.4, 0.58),  # middle points
+        (0.58, 0.1, -0.4, 0.448),  # middle points
+        (0.8, 0.1, None, 0.82),  # middle points
     ],
 )
-def test_extend_mean(given_percent: float, given_new_value: float, expected: float):
-    given = extend_mean(percent=given_percent, new_value=given_new_value)
+def test_extend_mean(
+    given_percent: float, given_new_value: float, given_shift, expected: float
+):
+    given = extend_mean(
+        percent=given_percent, new_value=given_new_value, shift=given_shift
+    )
     assert isclose(expected, given)
