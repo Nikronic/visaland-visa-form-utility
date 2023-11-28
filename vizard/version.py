67f7535864108192cbc2ea50eed6645c88733e39
Nikan Doosti <nikan.doosti@outlook.com>
2023-11-28 09:31:10 +0330

    bump version to `vizard-v0.25.0`

--- vizard/version.py
+++ vizard/version.py
@@ -1,8 +1,8 @@
 __all__ = ["VERSION_SHORT", "VERSION"]
 
 _MAJOR = "0"
-_MINOR = "24"
-_REVISION = "1"
+_MINOR = "25"
+_REVISION = "0"
 
 VERSION_SHORT = f"{_MAJOR}.{_MINOR}"
 """Version of the package as ``major.minor``
