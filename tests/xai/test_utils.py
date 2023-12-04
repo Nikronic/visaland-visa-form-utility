e6185a4c5bb99b774d6622775ca7a2983d79df76
Nikan Doosti <nikan.doosti@outlook.com>
2023-12-04 16:50:40 +0330

    add tests

--- tests/xai/test_utils.py
+++ tests/xai/test_utils.py
@@ -1,3 +1,7 @@
+from typing import List
+
+from pytest import mark
+
 from vizard.data import constant
 from vizard.xai import utils
 
@@ -31,3 +35,36 @@ def test_logical_order():
     correct_order_question = "child_count"
     check = utils.logical_order(question_title, utils.logical_dict, is_answered)
     assert check == correct_order_question, "test logical order has been failed"
+
+
+@mark.parametrize(
+    argnames=[
+        "given_suggested",
+        "given_parameter_builder_instances_names",
+        "given_len_user_answered_params",
+        "given_len_logically_answered_params",
+        "expected_suggestion",
+    ],
+    argvalues=[
+        ("", ["x", "y"], 2, 2, "y"),
+        ("z", ["x", "y"], 3, 2, "z"),
+        ("z", [], 2, 2, "z"),
+        ("", [], 2, 2, ""),
+    ],
+)
+def test_append_parameter_builder_instances(
+    given_suggested: str,
+    given_parameter_builder_instances_names: List[str],
+    given_len_user_answered_params: int,
+    given_len_logically_answered_params: int,
+    expected_suggestion: str,
+):
+    assert (
+        utils.append_parameter_builder_instances(
+            suggested=given_suggested,
+            parameter_builder_instances_names=given_parameter_builder_instances_names,
+            len_user_answered_params=given_len_user_answered_params,
+            len_logically_answered_params=given_len_logically_answered_params,
+        )
+        == expected_suggestion
+    )
