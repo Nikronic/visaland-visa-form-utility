52f7d93f84771ca1a461e7c76a074d25d84da9b6
Nikan Doosti <nikan.doosti@outlook.com>
2023-12-27 15:57:24 +0330

    do some best practices

--- tests/models/preprocessors/test_core.py
+++ tests/models/preprocessors/test_core.py
@@ -6,17 +6,20 @@ import pytest
 
 from vizard.models.preprocessors.core import PandasTrainTestSplit, TrainTestEvalSplit
 
+
 # Sample DataFrame
-data = {
-    "feature1": list(np.arange(start=1, stop=11)),
-    "feature2": list(np.arange(start=11, stop=21)),
-    "target": [0, 1, 1, 1, 0, 0, 0, 1, 0, 0],
-}
-df = pd.DataFrame(data)
+@pytest.fixture
+def sample_df():
+    data = {
+        "feature1": list(np.arange(start=1, stop=11)),
+        "feature2": list(np.arange(start=11, stop=21)),
+        "target": [0, 1, 1, 1, 0, 0, 0, 1, 0, 0],
+    }
+    return pd.DataFrame(data)
 
 
 @patch("vizard.models.preprocessors.core.TrainTestEvalSplit.set_configs")
-def test_train_test_eval_split(mock_set_configs):
+def test_train_test_eval_split(mock_set_configs, sample_df):
     # Mocked configuration for testing
     mock_config = {
         "test_ratio": 0.20,
@@ -28,7 +31,7 @@ def test_train_test_eval_split(mock_set_configs):
     mock_set_configs.return_value = mock_config
 
     splitter = TrainTestEvalSplit()
-    x_train, x_test, x_eval, y_train, y_test, y_eval = splitter(df, "target")
+    x_train, x_test, x_eval, y_train, y_test, y_eval = splitter(sample_df, "target")
 
     assert len(x_train) == 7  # 70% of data
     assert len(x_test) == 2  # 20% of data
@@ -39,9 +42,9 @@ def test_train_test_eval_split(mock_set_configs):
     assert y_eval.shape[0] == 1
 
     # Assert correct splitting with stratification
-    df["stratify_col"] = [0, 0, 0, 1, 1, 1, 1, 1, 1, 1]
+    sample_df["stratify_col"] = [0, 0, 0, 1, 1, 1, 1, 1, 1, 1]
     splitter = TrainTestEvalSplit(stratify="stratify_col")
-    x_train, x_test, x_eval, y_train, y_test, y_eval = splitter(df, "target")
+    x_train, x_test, x_eval, y_train, y_test, y_eval = splitter(sample_df, "target")
     assert pd.Series(y_train).value_counts().to_dict() == {0: 4, 1: 3}
     assert pd.Series(y_test).value_counts().to_dict() == {0: 1, 1: 1}
     assert pd.Series(y_eval).value_counts().to_dict() == {0: 1}
@@ -51,20 +54,20 @@ def test_train_test_eval_split(mock_set_configs):
     assert isinstance(y_train, np.ndarray)
 
     # Test with explicit random_state and stratify
-    splitter = TrainTestEvalSplit(random_state=100, stratify=df["stratify_col"])
-    x_train, x_test, x_eval, y_train, y_test, y_eval = splitter(df, "target")
+    splitter = TrainTestEvalSplit(random_state=100, stratify=sample_df["stratify_col"])
+    x_train, x_test, x_eval, y_train, y_test, y_eval = splitter(sample_df, "target")
     assert pd.Series(y_train).value_counts().to_dict() == {0: 4, 1: 3}
 
     # Test with eval_ratio = 0
     mock_config["eval_ratio"] = 0.0
     splitter = TrainTestEvalSplit()
-    x_train, x_test, y_train, y_test = splitter(df, "target")
+    x_train, x_test, y_train, y_test = splitter(sample_df, "target")
     assert len(x_train) == 8
     assert len(x_test) == 2
 
 
 @patch("vizard.models.preprocessors.core.PandasTrainTestSplit.set_configs")
-def test_pandas_train_test_split(mock_set_configs):
+def test_pandas_train_test_split(mock_set_configs, sample_df):
     mock_config = {
         "train_ratio": 0.80,
         "shuffle": True,
@@ -74,25 +77,25 @@ def test_pandas_train_test_split(mock_set_configs):
     mock_set_configs.return_value = mock_config
 
     splitter = PandasTrainTestSplit()
-    train_df, test_df = splitter(df, "target")
+    train_df, test_df = splitter(sample_df, "target")
 
     assert train_df.shape[0] == 8  # 80% of data
     assert test_df.shape[0] == 2  # 20% of data
 
     # test equality if seed is the same even if shuffle true
     splitter = PandasTrainTestSplit()
-    train_df1, test_df1 = splitter(df, "target")
-    train_df2, test_df2 = splitter(df, "target")  # Repeat with same random_state
+    train_df1, test_df1 = splitter(sample_df, "target")
+    train_df2, test_df2 = splitter(sample_df, "target")  # Repeat with same random_state
 
     assert train_df1.equals(train_df2)
     assert test_df1.equals(test_df2)
 
     # test equality if seed is the same even if shuffle true
     splitter = PandasTrainTestSplit()
-    train_df1, test_df1 = splitter(df, "target")
+    train_df1, test_df1 = splitter(sample_df, "target")
     mock_config["random_state"] = 85
     splitter2 = PandasTrainTestSplit()
-    train_df2, test_df2 = splitter2(df, "target")
+    train_df2, test_df2 = splitter2(sample_df, "target")
 
     assert not train_df1.equals(train_df2)
     assert not test_df1.equals(test_df2)
